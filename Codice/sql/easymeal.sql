5bc6ccf81d74cd5c1658215f95e1f0f586f8c954
lumine <hakupisi>
2024-01-14 18:56:54 +0100

    facendo docker-compose build viene rigenerato il database

--- Codice/sql/easymeal.sql
+++ Codice/sql/easymeal.sql
@@ -21,6 +21,10 @@ SET time_zone = "+00:00";
 -- Database: `easymeal`
 --
 
+DROP DATABASE IF EXISTS `easymeal`;
+CREATE DATABASE `easymeal`;
+USE `easymeal`;
+
 -- --------------------------------------------------------
 
 --
