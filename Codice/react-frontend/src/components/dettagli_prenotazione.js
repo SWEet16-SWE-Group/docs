4595069fcd099d3ea1726c345f71eb2a652e6638
albyx97 <albyx97@gmail.com>
2024-01-24 11:39:21 +0100

    Completato pagina di dettagli della prenotazione

--- Codice/react-frontend/src/components/dettagli_prenotazione.js
+++ Codice/react-frontend/src/components/dettagli_prenotazione.js
@@ -12,18 +12,25 @@ class DettagliPrenotazione extends Component {
         ordinazioni: [],
         ingredienti: [],
         rimossi: [],
-        quantita: 1
+        quantita: [],
+        ripristina: ""
     };
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.handlePrenotazione = this.handlePrenotazione.bind(this);
     this.rimuoviParole = this.rimuoviParole.bind(this);
+    this.calcolaFrequenza = this.calcolaFrequenza.bind(this);
    }
 
    componentDidMount() {
     let pathname = window.location.pathname;
     let id_prenotazione = pathname.split('/')[2];
 
-    axios.post("http://localhost:8888/select_prenotazione.php ", { id_prenotazione }).then(response =>
-      this.setState({ prenotazione: response.data}, () => {
+    axios.post("http://localhost:8888/select_prenotazione.php ", { id_prenotazione }).then(response => {
+      let stato=false;
+      if(response.data[0].Stato!==null)
+      {
+        stato=true;
+      }
+      this.setState({ prenotazione: response.data, ripristina: stato}, () => {
         if(this.state.prenotazione[0]!==null)
         {
           axios.post('http://localhost:8888/select_multiple_ordine_confermato.php', { id_prenotazione }).then(response => {
@@ -52,7 +59,8 @@ class DettagliPrenotazione extends Component {
             })
             })
         }
-    }))
+    })
+  })
   }
 
   rimuoviParole = () => {
@@ -65,38 +73,52 @@ class DettagliPrenotazione extends Component {
       return nuoviIngredienti.join(", ");
     });
 
-    this.setState({ ingredienti: nuovoArrayIngredienti });
+    this.setState({ ingredienti: nuovoArrayIngredienti }, () => {
+      this.calcolaFrequenza();
+    })
   };
 
-  handleSubmit = (event) => {
+  calcolaFrequenza = () => {
+    const { ingredienti } = this.state;
+  
+    const testoIngredienti = ingredienti.join(', ');
+    const parole = testoIngredienti.split(', ');
+  
+    const frequenzaParole = {};
+    parole.forEach(parola => {
+      if (!frequenzaParole[parola]) {
+        frequenzaParole[parola] = 1;
+      } else {
+        frequenzaParole[parola]++;
+      }
+    });
+  
+    const arrayQuantita = Object.keys(frequenzaParole).map(parola => ({ parola, frequenza: frequenzaParole[parola] }));
+  
+    this.setState({ quantita: arrayQuantita });
+  };
 
+  handlePrenotazione = (stato) => (event) => {
     event.preventDefault();
-
-    const cliente = JSON.parse(localStorage.getItem('idc'));
-    const tavolo = this.state.prenotazione[0].Id_tavolo;
-    const ristorante = this.state.prenotazione[0].Id_ristorante;
-    const prodotto = this.state.pietanza[0].ID_prodotto;
-    const porzioni = this.state.quantita;
-    const tot = this.state.pietanza[0].Prezzo * porzioni;
-    const rimossi = this.state.rimossi.join(', ')
-
-
-    const insert = [
+    const prenotazione = this.state.prenotazione[0].ID_prenotazione;
+    const aggiorna = [
       {
-        id_cliente: cliente,
-        id_tavolo: tavolo,
-        id_ristorante: ristorante,
-        id_prodotto: prodotto,
-        quantita: porzioni,
-        totale: tot,
-        ingredienti_rimossi: rimossi
+        id_prenotazione: prenotazione,
+        stato: stato,
       }
     ]
+    axios.post("http://localhost:8888/aggiorna_stato_prenotazione.php ", aggiorna[0]).then(() => {
+      if(stato!==3) 
+      {
+        this.setState({ ripristina: true });
+      }
+      else
+      {
+        this.setState({ ripristina: false });
+      }
+    })
 
-    axios
-      .post("http://localhost:8888/insert_ordine.php", insert[0])
-
-  };
+  }
 
   render() {
 
@@ -104,19 +126,19 @@ class DettagliPrenotazione extends Component {
       <>
       <Navbar key="navbar-key" />
       <div className="container-fluid p-auto border width-95 rounded border-2 margin-tb h-auto">
-      {this.state.prenotazione.length !== 0 && this.state.prenotazione.map((rs, index) => (
-            <div key={index} className="mt-5 d-flex justify-content-center">
-                <Link to={`/dashboardristoratori`} className="btn btn-outline-primary btn-outline btn-lg w-100 m-2">TORNA ALLE PRENOTAZIONI</Link>
-            </div>
-          ))}
+          <div className="mt-5 d-flex justify-content-center">
+              <Link to={`/dashboardristoratori`} className="btn btn-outline-primary btn-outline btn-lg w-100 m-2">TORNA ALLE PRENOTAZIONI</Link>
+          </div>
           <h1 className="my-5 d-flex justify-content-center">PRENOTAZIONE NUMERO: 
             {this.state.prenotazione && this.state.prenotazione.map((rs, index) => (
               <div className="mx-3" key={index}>{rs.ID_prenotazione}</div>
             ))} 
           </h1>
+          {this.state.ordinazioni.length !== 0 && (
+            <>
           <div className="row mb-5 mx-3">
             <div className="col-12" >
-              <table className="table-attive table">
+              <table className="table">
                 <thead className="thead-dark">
                   <tr>
                     <th>Pietanza</th>
@@ -142,13 +164,49 @@ class DettagliPrenotazione extends Component {
               </table>
           </div>
         </div>
+        <div className="row mb-5 mx-3">
+        <h3 className="my-5 d-flex justify-content-center">INVENTARIO PRENOTAZIONE</h3>
+        <div className="col-4" ></div>
+            <div className="col-4" >
+              <table className="table">
+                <thead className="thead-dark">
+                  <tr>
+                    <th>Quantità</th>
+                    <th>Ingrediente</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {this.state.quantita.length !== 0 && this.state.quantita.map((rs, index) => (
+                    <tr key={index} >
+                      <td>{rs.frequenza}</td>
+                      <td>{rs.parola}</td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+          </div>
+          <div className="col-4" ></div>
+        </div>
+        </>
+        )}
         <div className="row my-5">
+          {this.state.prenotazione[0] && this.state.ripristina===false && (
+            <>
             <div className="col-6">
-                <button type="submit" className="btn btn-outline-success btn-lg w-100 my-2">ACCETTA</button>
+                <button type="button" className="btn btn-outline-success btn-lg w-100 my-2" onClick={this.handlePrenotazione(1)}>ACCETTA</button>
             </div>
             <div className="col-6">
-                <button type="submit" className="btn btn-outline-danger btn-lg w-100 my-2">RIFIUTA</button>
+                <button type="button" className="btn btn-outline-danger btn-lg w-100 my-2" onClick={this.handlePrenotazione(0)}>RIFIUTA</button>
             </div>
+            </>
+          )}
+          {this.state.prenotazione[0] && this.state.ripristina===true && (
+            <>
+            <div className="col-12">
+                <button type="button" className="btn btn-outline-primary btn-lg w-100 my-2" onClick={this.handlePrenotazione(3)}>RIPRISTINA</button>
+            </div>
+            </>
+          )}
         </div>
       </div>
       </>
