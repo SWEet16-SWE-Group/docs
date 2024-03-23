9137a4133b71ac5b5477df05fb7d3823419b6025
bzn0u95quudr7jutfjap <114179940+bzn0u95quudr7jutfjap@users.noreply.github.com>
2024-03-23 15:54:14 +0100

    stamnpa cose

--- rtb/make-glossario.sh
+++ rtb/make-glossario.sh
@@ -1,7 +1,36 @@
 #!/bin/bash
 
-function findsegnaposti(){
-  cat $( find . -type f -name *.tex ) | grep -ne '\$....\$'
+function texfiles(){
+  find . -type f -name *.tex
 }
 
-findsegnaposti
+function contains(){
+  [[ -n "$(grep -e $* )" ]]
+}
+
+function linenumber(){
+  SEGNAPOSTO="$1"
+  FILE="$2"
+  grep -ne "$SEGNAPOSTO" "$FILE" | sed 's/\([0-9]*\):/\1/g' | xargs -I ç printf "\t\t%06d\n" ç
+}
+
+function findsegnaposti(){
+  cat $( texfiles ) |
+    sed -z 's/\s/\n/g' |
+    grep -e '\$....\$' |
+    sed 's/\$....\$//;s/[;:,.)]*$//' |
+    sort |
+    uniq
+}
+
+function findcontainssegnaposto(){
+  SEGNAPOSTO="$*"
+  texfiles | xargs -I ç sh -c "contains $SEGNAPOSTO ç && printf '\t%s\n' ç # && linenumber $SEGNAPOSTO ç"
+}
+
+export -f texfiles
+export -f contains
+export -f linenumber
+export -f findcontainssegnaposto
+
+findsegnaposti | xargs -I ç sh -c "findcontainssegnaposto ç"
