9aa3a1ae77fdff7e998a3aa71d1db7bd1d5174b6
bzn0u95quudr7jutfjap <114179940+bzn0u95quudr7jutfjap@users.noreply.github.com>
2024-03-25 12:39:09 +0100

    rimosso teee

--- rtb/validatore-stile.sh
+++ rtb/validatore-stile.sh
@@ -41,7 +41,7 @@ function findreplace(){
     ; s/:(})? ([a-z])/:\1 \U\2/g
     ' "$line"
 
-    while [[ -n "$( grep -zP '\\item .*?\n[^\\]*?\n' "$line" | tee /dev/stdout )" ]] ; do
+    while [[ -n "$( grep -zP '\\item .*?\n[^\\]*?\n' "$line" )" ]] ; do
       perl -i -0pe '
       # mergia le linee di item
       ; s/(\\item .*?)\n([^\\]*?)\n/\1 \2\n/g
