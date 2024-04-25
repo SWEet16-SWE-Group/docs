81d0bc4760dd4b5c6c3999d483c8dbeee719aecc
bzn0u95quudr7jutfjap <114179940+bzn0u95quudr7jutfjap@users.noreply.github.com>
2024-04-25 17:17:31 +0200

    correzione dei test

--- mvp-codice/0-sql/tests.d/insert_cliente.sql
+++ mvp-codice/0-sql/tests.d/insert_cliente.sql
@@ -4,6 +4,6 @@ insert into profili values(2,1);
 insert into ristoratori values(2);
 
 select assert(insert_cliente(1) = 1) as "insert_cliente: nuovo                         " \G ;
-select assert(insert_cliente(1) = 1) as "insert_cliente: già esistente in clienti      " \G ;
+select assert(insert_cliente(1) = 0) as "insert_cliente: già esistente in clienti      " \G ;
 select assert(insert_cliente(2) = 0) as "insert_cliente: già esistente in ristoratori  " \G ;
 select assert(insert_cliente(3) = 0) as "insert_cliente: non esistente in profili      " \G ;
