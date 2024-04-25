c11dc878885181757cdfccbb585c1cad3cc879a2
bzn0u95quudr7jutfjap <114179940+bzn0u95quudr7jutfjap@users.noreply.github.com>
2024-04-25 13:14:54 +0200

    creazione delle tabelle

---
+++ mvp-codice/0-sql/main.sql
@@ -0,0 +1,21 @@
+
+drop database if exists easymeal;
+create database easymeal;
+use easymeal;
+
+
+create table accounts     ();
+create table profili      ();
+create table clienti      ();
+create table ristoratori  ();
+create table messaggi     ();
+create table recensioni   ();
+create table prenotazioni ();
+create table allrgeni     ();
+create table allergie     ();
+create table ingredienti  ();
+create table ricette      ();
+create table pietanze     ();
+create table ordinazioni  ();
+create table aggiunti     ();
+create table rimossi      ();
