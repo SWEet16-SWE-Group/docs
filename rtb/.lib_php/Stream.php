11021a1c648ff7cfbe3a1763910c7f27e5c82f8d
bzn0u95quudr7jutfjap <114179940+bzn0u95quudr7jutfjap@users.noreply.github.com>
2024-05-05 15:15:45 +0200

    extras

--- rtb/.lib_php/Stream.php
+++ rtb/.lib_php/Stream.php
@@ -8,12 +8,27 @@ function _filter($f) {
   return fn ($a) => array_filter($a, $f);
 }
 
+function _reduce($f, $i) {
+  return fn ($a) => array_reduce($a, $f, $i);
+}
+
 function _implode($d) {
   return fn ($a) => implode($d, $a);
 }
 
 function _explode($d) {
   return fn ($a) => explode($d, $a);
+}
+
+function _sort() {
+  return function ($a) {
+    sort($a);
+    return $a;
+  };
+}
+
+function _unique() {
+  return fn ($a) => array_unique($a);
 }
 
 function stream($a, ...$p) {
