acac163ff9d2ddc89106ef9073057523fcff3771
bzn0u95quudr7jutfjap <114179940+bzn0u95quudr7jutfjap@users.noreply.github.com>
2024-05-20 16:37:06 +0200

    correzione foreign keys

--- initdb/easymeal.sql
+++ initdb/easymeal.sql
@@ -12,14 +12,16 @@ create table account(
 
 create table clienti(
   id int not null auto_increment primary key,
-  account int not null references account(id),
+  account int not null,
+  foreign key (account) references account(id),
   nome varchar(255) not null unique
   -- decidere se nome globale tra tutti i clienti o solo nel contesto di un account
 );
 
 create table ristoratori(
   id int not null auto_increment primary key,
-  account int not null references account(id),
+  account int not null,
+  foreign key (account) references account(id),
   nome varchar(255) not null unique,
   indirizzo varchar(255) not null unique,
   telefono varchar(255) not null unique
@@ -32,39 +34,48 @@ create table allergeni(
 
 create table ingredienti(
   id int not null auto_increment primary key,
-  ristorante int not null references ristoratori(id),
+  ristorante int not null,
+  foreign key (ristorante) references ristoratori(id),
   nome varchar(255) not null,
   unique (ristorante, nome)
 );
 
 create table reagenti(
-  ingrediente int not null references ingredienti(id),
-  allergene   int not null references allergeni(id),
+  ingrediente int not null,
+  foreign key (ingrediente) references ingredienti(id),
+  allergene   int not null,
+  foreign key (allergene) references allergeni(id),
   primary key (ingrediente, allergene)
 );
 
 create table allergie(
-  cliente   int not null references clienti(id),
-  allergene int not null references allergeni(id),
+  cliente   int not null,
+  foreign key (cliente) references clienti(id),
+  allergene int not null,
+  foreign key (allergene) references allergeni(id),
   primary key (cliente, allergene)
 );
 
 create table pietanze(
   id int not null auto_increment primary key,
-  ristorante int not null references ristoratori(id),
+  ristorante int not null,
+  foreign key (ristorante) references ristoratori(id),
   nome varchar(255) not null,
   unique (ristorante, nome)
 );
 
 create table ricette(
-  pietanza    int not null references pietanze(id),
-  ingrediente int not null references ingredienti(id),
+  pietanza    int not null,
+  foreign key (pietanza) references pietanze(id),
+  ingrediente int not null,
+  foreign key (ingrediente) references ingredienti(id),
   primary key (pietanza, ingrediente)
 );
 
 create table prenotazioni(
   id int not null auto_increment primary key,
-  ristorante int not null references ristoratore(id),
+  ristorante int not null,
+  foreign key (ristorante) references ristoratori(id),
   timestamp datetime not null,
   n_inviti int not null,
   divisione_conto enum ('equa', 'proporzionale') not null
@@ -72,26 +83,29 @@ create table prenotazioni(
 
 create table inviti(
   id int not null auto_increment primary key,
-  prenotazione int not null references prenotazioni(id),
-  cliente   int not null references clienti(id),
+  prenotazione int not null,
+  foreign key (prenotazione) references prenotazioni(id),
+  cliente   int not null,
+  foreign key (cliente) references clienti(id),
   pagamento enum ('pagato', 'non_pagato') not null
   -- necessario se divisione equa
 );
 
 create table ordinazioni(
   id int not null auto_increment primary key,
-  invito int not null references inviti(id),
-  pietanza int not null references pientaze(id),
+  invito int not null,
+  foreign key (invito) references inviti(id),
+  pietanza int not null,
+  foreign key (pietanza) references pietanze(id),
   pagamento enum ('pagato', 'non_pagato') not null
   -- necessario se divisione proporzionale
 );
 
 create table dettagli_ordinazione(
-  ingrediente int not null references ingredienti(id),
-  ordinazione int not null references ordinazione(id),
+  ingrediente int not null,
+  foreign key (ingrediente) references ingredienti(id),
+  ordinazione int not null,
+  foreign key (ordinazione) references ordinazioni(id),
   dettaglio enum ('-', '+') not null,
   primary key (ingrediente, ordinazione)
 );
-
-
-insert into clienti(account, nome) values (12, 'aaaaa');
