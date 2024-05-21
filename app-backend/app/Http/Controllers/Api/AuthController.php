76438d70505ca669ebd5014a4419fa0ae7f62759
alberto.michelazzo <alberto.michelazzo@studenti.unipd.it>
2024-05-21 14:37:50 +0200

    commit

---
+++ app-backend/app/Http/Controllers/Api/AuthController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\LoginRequest;
+use App\Http\Requests\SignupRequest;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class AuthController extends Controller
+{
+    public function signup(SignupRequest $request)
+    {
+        $data = $request->validated();
+        /** @var User $user */
+        User::create([
+            'email' => $data['email'],
+            'password' => bcrypt($data['password']),
+        ]);
+
+        $token = $user->createToken('main')->plainTextToken;
+
+        return response(compact('user', 'token'));
+    }
+
+    public function login(LoginRequest $request)
+    {
+        $credentials = $request->validated();
+        if (!Auth::attempt($credentials)) {
+            return response([
+                'message' => 'Email o password inseriti non sono corretti'
+            ]);
+        }
+
+        /** @var User $user */
+        $user = Auth::user();
+        $token = $user->createToken('main')->plainTextToken;
+
+        return response(compact('user', 'token'));
+    }
+
+    public function logout(Request $request)
+    {
+        /** @var User $user */
+        $user = $request->user();
+        $user->currentAccessToken()->delete();
+
+        return response('', 204);
+    }
+
+}
--- app-frontend/src/views/SignUp.jsx
+++ app-frontend/src/views/SignUp.jsx
@@ -1,9 +1,39 @@
 import {Link} from "react-router-dom";
+import {useRef, useState} from "react";
+import axiosClient from "../axios-client";
+import {useStateContext} from "../contexts/ContextProvider";
 
 export default function SignUp() {
 
+    const emailRef = useRef();
+    const passwordRef = useRef();
+    const passwordConfirmationRef = useRef();
+
+    const [errors, setErrors] = useState(null)
+
+    const {setUser, setToken} = useStateContext()
+
     const onSubmit = (ev) => {
         ev.preventDefault()
+        const payload = {
+            email: emailRef.current.value,
+            password: passwordRef.current.value,
+            password_confirmation: passwordConfirmationRef.current.value,
+        }
+        console.log(payload);
+        axiosClient.post('/signup', payload)
+            .then(({data}) => {
+                debugger;
+                setUser(data.user)
+                setToken(data.token)
+            })
+            .catch(err => {
+                const response = err.response;
+            if(response && response.status === 422) {
+                    console.log(response.data.errors);
+                    setErrors(response.data.errors);
+                }
+            })
     }
 
     return (
@@ -11,9 +41,16 @@ export default function SignUp() {
             <div className="form">
                 <form onSubmit={onSubmit}>
                     <h1 className="title">Registrazione account</h1>
-                    <input type="email" placeholder=" Email" />
-                    <input type="password" placeholder=" Password"/>
-                    <input type="password" placeholder=" Ripeti password"/>
+
+                    {errors && <div className="alert">
+                        {Object.keys(errors).map(key => (
+                            <p key={key}>{errors[key][0]}</p>
+                            ))}
+                    </div>
+                    }
+                    <input ref={emailRef} type="email" placeholder=" Email" />
+                    <input ref={passwordRef} type="password" placeholder=" Password"/>
+                    <input ref={passwordConfirmationRef} type="password" placeholder=" Ripeti password"/>
                     <button className="btn btn-block">Registrati</button>
                     <p className="message">
                         Sei già registrato?  <Link to="/login"> Vai a login</Link>
