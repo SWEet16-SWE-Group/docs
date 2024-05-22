349efc5471f21f6cc40c6e4cd5ac644922ccd900
alberto.michelazzo <alberto.michelazzo@studenti.unipd.it>
2024-05-22 16:07:29 +0200

    nome corretto

--- app-backend/app/Http/Controllers/Api/AuthController.php
+++ app-backend/app/Http/Controllers/Api/AuthController.php
@@ -5,7 +5,8 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginRequest;
 use App\Http\Requests\SignupRequest;
-use App\Models\Account;
+use App\Models\User;
+use http\Env\Response;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -13,17 +14,14 @@ class AuthController extends Controller
 {
     public function signup(SignupRequest $request)
     {
-
         $data = $request->validated();
-        /** @var Account $user */
-
-        $user = Account::create([
+        /** @var User $user */
+        $user = User::create([
             'email' => $data['email'],
             'password' => bcrypt($data['password']),
         ]);
 
         $token = $user->createToken('main')->plainTextToken;
-
         return response(compact('user', 'token'));
     }
 
@@ -32,24 +30,21 @@ class AuthController extends Controller
         $credentials = $request->validated();
         if (!Auth::attempt($credentials)) {
             return response([
-                'message' => 'Email o password inseriti non sono corretti'
-            ]);
+                'message' => 'Email e/o password non corretti'
+            ], 422);
         }
 
-        /** @var Account $user */
+        /** @var User $user */
         $user = Auth::user();
         $token = $user->createToken('main')->plainTextToken;
-
         return response(compact('user', 'token'));
     }
 
     public function logout(Request $request)
     {
-        /** @var Account $user */
+        /** @var User $user */
         $user = $request->user();
         $user->currentAccessToken()->delete();
-
         return response('', 204);
     }
-
 }
