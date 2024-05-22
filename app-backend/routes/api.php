e8373a1820595354a9b1b4b81dc1ad797f6dd1d9
alberto.michelazzo <alberto.michelazzo@studenti.unipd.it>
2024-05-22 16:07:03 +0200

    aggiunto gruppo per poter effettuare logout

--- app-backend/routes/api.php
+++ app-backend/routes/api.php
@@ -15,10 +15,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
+Route::middleware('auth:api')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+    Route::post('/logout', [AuthController::class, 'logout']);
 });
 
 Route::post('/signup', [AuthController::class, 'signup']);
 Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout']);
+
