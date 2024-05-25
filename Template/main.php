1944090a0b9dbee5f1e502f40c8a930c55b928ca
bzn0u95quudr7jutfjap <114179940+bzn0u95quudr7jutfjap@users.noreply.github.com>
2024-05-25 14:49:02 +0200

    main.tex -> main.php

--- Template/main.php
+++ Template/main.php
@@ -1,3 +1,9 @@
+<?php
+ob_start();
+ob_start(function ($tex) {
+  return $tex;
+});
+?>
 \nonstopmode
 \documentclass[a4paper, 11pt]{article}
 \usepackage{graphicx} % Required for inserting images
@@ -7,17 +13,17 @@
 \usepackage{setspace}
 \usepackage{array}
 \usepackage[usenames,dvipsnames]{xcolor}
-\usepackage{colortbl} 
+\usepackage{colortbl}
 \usepackage{tabularray}
 \usepackage[italian]{babel}
 
- \geometry{
- a4paper,
- left=25mm,
- right=25mm,
- top=20mm,
- bottom=20mm,
- }
+\geometry{
+a4paper,
+left=25mm,
+right=25mm,
+top=20mm,
+bottom=20mm,
+}
 
 \setlength{\parskip}{1em}
 \setlength{\parindent}{0pt}
@@ -26,7 +32,7 @@
 \begin{document}
 
 \begin{minipage}{0.35\linewidth}
-    \includegraphics[width=\linewidth]{Logo_Università_Padova.svg.png}
+\includegraphics[width=\linewidth]{Logo_Università_Padova.svg.png}
 \end{minipage}\hfil
 \begin{minipage}{0.55\linewidth}
 \textbf{Università degli Studi di Padova} \\
@@ -38,11 +44,11 @@ Anno Accademico 2023/2024
 \vspace{5mm}
 
 \begin{minipage}{0.35\linewidth}
-    \includegraphics[width=\linewidth]{logo rotondo.jpg}
+\includegraphics[width=\linewidth]{logo rotondo.jpg}
 \end{minipage}\hfil
 \begin{minipage}{0.55\linewidth}
 \textbf{Gruppo:} SWEet16 \\
-\textbf{Email:} 
+\textbf{Email:}
 \href{mailto:sweet16.unipd@gmail.com}{\nolinkurl{sweet16.unipd@gmail.com}}
 \end{minipage}
 
@@ -50,9 +56,9 @@ Anno Accademico 2023/2024
 
 \begin{center}
 \begin{Huge}
-        \textbf{Titolo del documento} \\
-        \vspace{4mm}
-        
+\textbf{Titolo del documento} \\
+\vspace{4mm}
+
 \end{Huge}
 
 \vspace{20mm}
@@ -60,11 +66,11 @@ Anno Accademico 2023/2024
 \begin{large}
 \begin{spacing}{1.4}
 \begin{tabular}{c c c}
-   Redattori:  &  Alberto M. & \\
-   Verificatori: & Iulius S. &  \\
-   Amministratore: & Alberto C. & \\
-   Destinatari: & T. Vardanega & R. Cardin \\  
-   Versione: & 0.1.0 & 
+Redattori: & Alberto M. & \\
+Verificatori: & Iulius S. & \\
+Amministratore: & Alberto C. & \\
+Destinatari: & T. Vardanega & R. Cardin \\
+Versione: & 0.1.0 &
 \end{tabular}
 \end{spacing}
 \end{large}
@@ -73,7 +79,7 @@ Anno Accademico 2023/2024
 \pagebreak
 
 \begin{huge}
-    \textbf{Registro delle modifiche}
+\textbf{Registro delle modifiche}
 \end{huge}
 \vspace{5pt}
 
@@ -83,14 +89,26 @@ row{odd}={bg=white},
 row{even}={bg=lightgray},
 row{1}={bg=black,fg=white}
 }
-    Versione & Data & Autore & Verificatore & Descrizione \\
-    0.1.0 & 2023/10/30 & Mario & Luigi & Approvazione per rilascio \\
-    \hline
-  
+Versione & Data & Autore & Verificatore & Descrizione \\
+0.1.0 & 2023/10/30 & Mario & Luigi & Approvazione per rilascio \\
+\hline
+
 \end{tblr}
 
 \pagebreak
 \tableofcontents
-\pagebreak 
+\pagebreak
 
 \end{document}
+<?php
+ob_end_flush();
+$tex = ob_get_contents();
+ob_end_clean();
+
+$opts = getopt('p');
+if (array_key_exists('p', $opts)) {
+  echo $tex;
+} else {
+  chdir(__DIR__);
+  file_put_contents('main.tex', $tex);
+}
