f9d026c9d630ce9468dc9251c7a46651dcf81952
bzn0u95quudr7jutfjap <114179940+bzn0u95quudr7jutfjap@users.noreply.github.com>
2024-05-25 15:56:55 +0200

    errori belli

--- .libphp/utils.php
+++ .libphp/utils.php
@@ -1,5 +1,21 @@
 <?php
 
+// Errori belli
+
+set_error_handler(function ($severity, $message, $file, $line) {
+  throw new \ErrorException($message, $severity, $severity, $file, $line);
+});
+
+set_exception_handler(function (Throwable $exception) {
+  $a = explode("\n", "{$exception->getMessage()}\n\n{$exception->getTraceAsString()}");
+  $format = '%-' . max(array_map('strlen', $a)) . 's' . "        <br>\n";
+  $a = array_map(fn ($a) => sprintf($format, $a), $a);
+  $a = implode("", $a);
+  die("\n" . $a . "\n");
+});
+
+// Utils
+
 function str_replace_array($a, $s) {
   return str_replace(array_keys($a), $a, $s);
 }
