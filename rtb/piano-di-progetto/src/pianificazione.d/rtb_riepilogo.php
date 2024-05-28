ddc99f0d38726c54fc3d8b1721468aa9c85b595a
bzn0u95quudr7jutfjap <114179940+bzn0u95quudr7jutfjap@users.noreply.github.com>
2024-05-28 20:21:38 +0200

    anche i riepilogi sono preprocessati

--- rtb/piano-di-progetto/src/pianificazione.d/rtb_riepilogo.php
+++ rtb/piano-di-progetto/src/pianificazione.d/rtb_riepilogo.php
@@ -1,89 +1,60 @@
+<?php
+
+require_once __DIR__ . '/pb.php';
+
+function somma3d($a, $rows, $cols) {
+  return array_combine($rows, array_map(fn ($r) => array_map(fn ($c) => array_sum(array_column(array_column($a, $r), $c)), $cols), $rows));
+}
+
+$membri = [
+  (string)alberto_c(),
+  (string)bilal_em(),
+  (string)alberto_m(),
+  (string)alex_s(),
+  (string)iulius_s(),
+  (string)giovanni_z(),
+];
+
+$rtb_somma_preventivi = somma3d(array_column($periodi_rtb, preventivo), $membri, range(0, 5));
+$rtb_somma_consuntivi = somma3d(array_column($periodi_rtb, consuntivo), $membri, range(0, 5));
+
+
+?>
+
 \nonstopmode
 \textbf{Preventivo economico totale}
 
-\begin{tblr}{
-    colspec={|X[5cm]|X[3.5cm]|X[1.5cm]|X[3.5cm]},
-    row{odd}={bg=white},
-    row{even}={bg=lightgray},
-    row{1}={bg=black, fg=white},
-    row{8}={bg=black, fg=white}
-}
+<?php
 
-    Ruolo & Costo orario (€/h) & N. Ore & Costo totale (€) \\ \hline
-    Responsabile & 30,00 & 21 & 630,00 \\ \hline
-    Amministratore & 20,00 & 18 & 360,00 \\ \hline
-    Analista & 25,00 & 67 & 1675,00 \\ \hline
-    Progettista & 25,00 & 26 & 650,00 \\ \hline
-    Programmatore & 15,00 & 30 & 450,00 \\ \hline
-    Verificatore & 15,00 & 49 & 735,00 \\ \hline
-    Totale & \SetCell[c=1]{c} & 211 & 4500,00 \\ \hline
+echo str_replace_array(['SOLDI' => tabella_to_string(tabella_soldi($rtb_somma_preventivi))],  tabella_soldi);
 
-    \end{tblr}
+?>
 
 \textbf{Consuntivo economico finale}
 
-\begin{tblr}{
-    colspec={|X[5cm]|X[3.5cm]|X[1.5cm]|X[3.5cm]},
-    row{odd}={bg=white},
-    row{even}={bg=lightgray},
-    row{1}={bg=black, fg=white},
-    row{8}={bg=black, fg=white}
-}
+<?php
 
-    Ruolo & Costo orario (€/h) & N. Ore & Costo totale (€) \\ \hline
-    Responsabile & 30,00 & 19 & 570,00 \\ \hline
-    Amministratore & 20,00 & 20 & 400,00 \\ \hline
-    Analista & 25,00 & 70 & 1750,00 \\ \hline
-    Progettista & 25,00 & 30 & 750,00 \\ \hline
-    Programmatore & 15,00 & 40 & 600,00 \\ \hline
-    Verificatore & 15,00 & 61 & 915,00 \\ \hline
-    Totale & \SetCell[c=1]{c} & 240 & 4985,00 \\ \hline
+echo str_replace_array(['SOLDI' => tabella_to_string(tabella_soldi($rtb_somma_consuntivi))],  tabella_soldi);
 
-\end{tblr}
+?>
 
 \pagebreak
 
 \textbf{Preventivo orario totale}
 
-\begin{tblr}{
-    colspec={|X[5cm]|X[.5cm]|X[.5cm]|X[.5cm]|X[.5cm]|X[.5cm]|X[.5cm]|X[3.5cm]},
-    row{odd}={bg=white},
-    row{even}={bg=lightgray},
-    row{1}={bg=black, fg=white},
-    row{8}={bg=black, fg=white}
-}
+<?php
 
-    Nominativo & Re & Am & An & Pg & Pr & Vf & Ore Totali \\ \hline
-    Alberto C. & - & - & 5 & 6 & 15 & 6 & 32 \\ \hline
-    Bilal El M. & 4 & 5 & 13 & 6 & - & 15 & 43 \\ \hline
-    Alberto M. & 8 & 2 & 19 & - & - & 7 & 36 \\ \hline
-    Alex S. & 4 & 11 & 5 & 5 & 5 & 5 & 35 \\ \hline
-    Iulius S. & - & - & 25 & - & - & 10 & 35 \\ \hline
-    Giovanni Z. & 5 & - & - & 9 & 10 & 6 & 30 \\ \hline
-    Totale & 21 & 18 & 67 & 26 & 30 & 49 & 211\\ \hline
+echo str_replace_array(['ORE'   => tabella_to_string(tabella_ore($rtb_somma_preventivi))],    tabella_ore);
 
-\end{tblr}
+?>
 
 \textbf{Consuntivo orario finale}
 
-\begin{tblr}{
-    colspec={|X[5cm]|X[.5cm]|X[.5cm]|X[.5cm]|X[.5cm]|X[.5cm]|X[.5cm]|X[3.5cm]},
-    row{odd}={bg=white},
-    row{even}={bg=lightgray},
-    row{1}={bg=black, fg=white},
-    row{8}={bg=black, fg=white}
-}
+<?php
 
-    Nominativo & Re & Am & An & Pg & Pr & Vf & Ore Totali \\ \hline
-    Alberto C. & - & 2 & 3 & 6 & 20 & 9 & 40\\ \hline
-    Bilal El M. & 2 & - & 10 & 6 & - & 11 & 29\\ \hline
-    Alberto M. & 5 & 5 & 21 & - & - & 10 & 41\\ \hline
-    Alex S. & 7 & 13 & 10 & 5 & 5 & 10 & 50\\ \hline
-    Iulius S. & - & - & 23 & 4 & - & 13 & 40\\ \hline
-    Giovanni Z. & 5 & - & 3 & 9 & 15 & 8 & 40\\ \hline
-    Totale & 19 & 20 & 70 & 30 & 40 & 61 & 240\\ \hline
+echo str_replace_array(['ORE'   => tabella_to_string(tabella_ore($rtb_somma_consuntivi))],    tabella_ore);
 
-\end{tblr}
+?>
 
 \paragraph{Gestione dei ruoli}
 Durante la fase di RTB, il 29\% delle risorse orario è stato dedicato al ruolo di Analista, il 17\% a quello di Programmatore, il 25\% a quello di Verificatore, mentre
