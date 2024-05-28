7da3ce34dbec23d5de1b8a4e89a11f9beb0796c4
Alberto Michelazzo <alby.michelazzo@gmail.com>
2024-05-28 12:21:09 +0200

    aggiunti template per routes laravel

--- app-backend/routes/api.php
+++ app-backend/routes/api.php
@@ -1,6 +1,9 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Middleware\UserIsClient;
+use App\Http\Middleware\UserIsRestaurant;
+use App\Http\Middleware\UserIsAuthenticated;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,11 +19,32 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::middleware('auth:api')->group(function () {
-    Route::get('/user', function (Request $request) {
-        return $request->user();
-    });
+
+    Route::middleware(UserIsAuthenticated::class) {
+    Route::prefix('authenticated')->group(function () {})
+        // inserire qui dentro chiamate api per utente autenticato e solo utente autenticato
+
+    };
+
+    Route::middleware(UserIsRestaurant::class) {
+        Route::prefix('restaurant')->group(function () {})
+        // inserire qui dentro chiamate api per ristoratore e solo ristoratore
+
+    };
+
+    Route::middleware(UserIsClient::class) {
+    Route::prefix('client')->group(function () {})
+        // inserire qui dentro chiamate api per cliente e solo cliente
+
+    };
+
+
+    // inserire qui le chiamate api comuni a tutti e tre i tipi di utenti (ad esempio logout)
     Route::post('/logout', [AuthController::class, 'logout']);
 });
+
+
+// inserire qui le chiamate per gli utenti non autenticati
 
 Route::post('/signup', [AuthController::class, 'signup']);
 Route::post('/login', [AuthController::class, 'login']);
