aad39fef9e9bc1f72bb5d9f00068584511da25e5
bzn0u95quudr7jutfjap <114179940+bzn0u95quudr7jutfjap@users.noreply.github.com>
2024-05-31 22:41:47 +0200

    \\ \\hline -> ")

--- rtb/piano-di-qualifica/src/registro-modifiche.php
+++ rtb/piano-di-qualifica/src/registro-modifiche.php
@@ -5,13 +5,13 @@ row{odd}={bg=white},
 row{even}={bg=lightgray},
 row{1}={bg=black, fg=white}
 }
-Versione, Data, Autore, Verificatore, Descrizione \\ \hline
-1.0.0, 2024/04/15, alex_s(), "", Approvazione per il rilascio \\ \hline
-0.6.1, 2024/04/10, alberto_c(), alex_s(), Modifica grafici resoconto attività di verifica \\ \hline
-0.6.0, 2024/04/08, giovanni_z(), alberto_m(), Stesura qualità di processo \\ \hline
-0.5.0, 2024/04/02, alberto_c(), alex_s(), Stesura resoconto delle attività di verifica \\ \hline
-0.4.0, 2024/03/25, giovanni_z(), alex_s(), Stesura qualità di prodotto \\ \hline
-0.3.0, 2024/03/22, alberto_c(), alberto_m(), Stesura test \\ \hline
-0.2.0, 2024/03/19, bilal_em(), alberto_m(), Stesura introduzione \\ \hline
-0.1.0, 2024/02/16, alex_s(), alberto_m(), Stesura scheletro \\ \hline
+Versione, Data, Autore, Verificatore, Descrizione")
+1.0.0, 2024/04/15, alex_s(), "", Approvazione per il rilascio")
+0.6.1, 2024/04/10, alberto_c(), alex_s(), Modifica grafici resoconto attività di verifica")
+0.6.0, 2024/04/08, giovanni_z(), alberto_m(), Stesura qualità di processo")
+0.5.0, 2024/04/02, alberto_c(), alex_s(), Stesura resoconto delle attività di verifica")
+0.4.0, 2024/03/25, giovanni_z(), alex_s(), Stesura qualità di prodotto")
+0.3.0, 2024/03/22, alberto_c(), alberto_m(), Stesura test")
+0.2.0, 2024/03/19, bilal_em(), alberto_m(), Stesura introduzione")
+0.1.0, 2024/02/16, alex_s(), alberto_m(), Stesura scheletro")
 \end{tblr}
