940b2f2671c0ec2d409ee22f43a29ba52ca43bf7
bzn0u95quudr7jutfjap <114179940+bzn0u95quudr7jutfjap@users.noreply.github.com>
2024-05-31 22:53:17 +0200

    ->log

--- rtb/piano-di-qualifica/src/registro-modifiche.php
+++ rtb/piano-di-qualifica/src/registro-modifiche.php
@@ -6,12 +6,12 @@ row{even}={bg=lightgray},
 row{1}={bg=black, fg=white}
 }
 Versione, Data, Autore, Verificatore, Descrizione")
-SX, "2024/04/15", alex_s(), "", "Approvazione per il rilascio")
-DX, "2024/04/10", alberto_c(), alex_s(), "Modifica grafici resoconto attività di verifica")
-CE, "2024/04/08", giovanni_z(), alberto_m(), "Stesura qualità di processo")
-CE, "2024/04/02", alberto_c(), alex_s(), "Stesura resoconto delle attività di verifica")
-CE, "2024/03/25", giovanni_z(), alex_s(), "Stesura qualità di prodotto")
-CE, "2024/03/22", alberto_c(), alberto_m(), "Stesura test")
-CE, "2024/03/19", bilal_em(), alberto_m(), "Stesura introduzione")
-CE, "2024/02/16", alex_s(), alberto_m(), "Stesura scheletro")
+->log(SX, "2024/04/15", alex_s(), "", "Approvazione per il rilascio")
+->log(DX, "2024/04/10", alberto_c(), alex_s(), "Modifica grafici resoconto attività di verifica")
+->log(CE, "2024/04/08", giovanni_z(), alberto_m(), "Stesura qualità di processo")
+->log(CE, "2024/04/02", alberto_c(), alex_s(), "Stesura resoconto delle attività di verifica")
+->log(CE, "2024/03/25", giovanni_z(), alex_s(), "Stesura qualità di prodotto")
+->log(CE, "2024/03/22", alberto_c(), alberto_m(), "Stesura test")
+->log(CE, "2024/03/19", bilal_em(), alberto_m(), "Stesura introduzione")
+->log(CE, "2024/02/16", alex_s(), alberto_m(), "Stesura scheletro")
 \end{tblr}
