a58a11727b376bbc8aad423768f6f3a9bcfbe54a
IuliusSignorelli <iulius.signorelli@studenti.unipd.it>
2024-05-31 15:04:42 +0200

    spostati file

--- app-frontend/src/index.js
+++ app-frontend/src/index.js
@@ -1,63 +1,6 @@
-<<<<<<< HEAD
-import React from 'react';
-import ReactDOM from "react-dom/client";
-import {createBrowserRouter,
-        RouterProvider,
-} from "react-router-dom";
-import './index.css';
-import * as serviceWorker from './serviceWorker';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min.js';
-import Account from "./views/Account";
-import Client,{loader as clientLoader} from "./views/Client";
-import ClientForm from "./components/NewClient";
-import EditClient from "./views/EditClient";
-import axios from 'axios';
-import 'bootstrap/dist/css/bootstrap.css';
-import { ContextProvider } from './contexts/ContextProvider';
-
-
-const router = createBrowserRouter([
-    {
-      path : "/",
-      element : <Account />,
-    },
-
-    {
-        path : "new",
-        element : <newClient />,
-    },
-      
-    {
-        path : ":clientId",
-        element : <Client />,
-    },
-            {
-                path : ":clientId/edit",
-                element : <EditClient/>,
-            },
-        
-    
-    
-
-  ]);
-ReactDOM.createRoot(document.getElementById('root')).render(
-    <React.StrictMode>
-        <ContextProvider>
-        <RouterProvider router={router} />
-        </ContextProvider>
-    </React.StrictMode>
-)
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
-=======
 export * from "./App";
 export * from "./main";
 export * from "./router";
 export * from "./App.test";
 export * from "./setupTests";
 export * from "./serviceWorker";
->>>>>>> mvp-main
--- app-frontend/src/main.jsx
+++ app-frontend/src/main.jsx
@@ -1,44 +1,3 @@
-<<<<<<< HEAD
-import * as React from "react";
-import * as ReactDOM from "react-dom/client";
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
-import "./index.css";
-import Account,{loader as rootLoader, action as rootAction} from "./views/Account";
-import EditClient from "./views/EditClient";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element:{
-        path:"/",
-        element:<Account />,
-        loader : rootLoader,
-        action :rootAction,
-        children : [
-
-  {
-    path:"account/:clientId",
-    element:<Client />,
-  },
-  {
-    path: "account/:clientId/edit",
-    element: <EditClient />,
-    loader: contactLoader,
-  },
-],
-},
-  }
-]);
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>
-);
-=======
 import React from "react";
 import ReactDOM from "react-dom/client";
 import {RouterProvider} from "react-router-dom";
@@ -55,4 +14,3 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         </ContextProvider>
     </React.StrictMode>
 )
->>>>>>> mvp-main
