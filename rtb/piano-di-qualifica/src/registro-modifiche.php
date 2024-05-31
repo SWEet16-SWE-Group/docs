abf438b522ee14ef1bd2e799eff9eea52ef8843f
bzn0u95quudr7jutfjap <114179940+bzn0u95quudr7jutfjap@users.noreply.github.com>
2024-05-31 22:45:22 +0200

    SX

--- rtb/piano-di-qualifica/src/registro-modifiche.php
+++ rtb/piano-di-qualifica/src/registro-modifiche.php
@@ -7,7 +7,7 @@ row{1}={bg=black, fg=white}
 }
 Versione, Data, Autore, Verificatore, Descrizione")
 1.0.0, "2024/04/15", alex_s(), "", "Approvazione per il rilascio")
-0.6.1, "2024/04/10", alberto_c(), alex_s(), "Modifica grafici resoconto attività di verifica")
+SX, "2024/04/10", alberto_c(), alex_s(), "Modifica grafici resoconto attività di verifica")
 0.6.0, "2024/04/08", giovanni_z(), alberto_m(), "Stesura qualità di processo")
 0.5.0, "2024/04/02", alberto_c(), alex_s(), "Stesura resoconto delle attività di verifica")
 0.4.0, "2024/03/25", giovanni_z(), alex_s(), "Stesura qualità di prodotto")
