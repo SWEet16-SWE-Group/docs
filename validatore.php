b88b00666b5e7c1fec945ee9743791d65bd8574b
bzn0u95quudr7jutfjap <114179940+bzn0u95quudr7jutfjap@users.noreply.github.com>
2024-05-31 12:45:01 +0200

    rimosso il codice vecchio

--- validatore.php
+++ validatore.php
@@ -5,10 +5,6 @@ require_once __DIR__ . '/.libphp/Stream.php';
 require_once __DIR__ . '/.libphp/Vocaboli.php';
 require_once __DIR__ . '/.libphp/Validatore.php';
 
-function _lazifier($fn, ...$args) {
-  return fn () => $fn(...$args);
-}
-
 function _execdir($dstdir, $fn) {
   $srcdir = getcwd();
   try {
@@ -92,87 +88,4 @@ if (!_compile()) {
 
 foreach (array_column($artefatti, 'compilatore') as $compilatore) {
   $compilatore();
-}
-die();
-
-// VECCHIO MAIN
-
-function main_correttore_ortografico_action($dict, $files) {
-  $a = stream($files, _map(fn ($a) => "\"$a\""), _implode(' '));
-  $e = shell_exec("hunspell -p $dict -d it_IT,en_US -l $a");
-  if ($e) {
-    print_r("\n\nCorreggere le parole evidenziate di seguito\n");
-    print_r($e);
-    die(14);
-  }
-}
-
-function main_compile($files) {
-  foreach ($files as $a) {
-    $pfx = fn ($s) => "Compilazione latex: $a: $s\n";
-    echo $pfx("INIZIO");
-
-    chdir(dirname($a));
-    ob_start(fn ($b) => stream($b, _explode("\n"), _filter(fn ($a) => strlen($a) > 0), _map($pfx), _implode("\n")));
-    passthru("mkdir -p .build/ && latexmk -interaction=nonstopmode -halt-on-error -output-directory=.build/ -pdf main.tex > .build/php.out", $e);
-    ob_end_flush();
-    echo $pfx("FINE");
-    echo $pfx("CODICE DI USCITA: $e");
-    if ($e != 0) {
-      echo file_get_contents('.build/main.log');
-      die($e);
-    }
-    isset($nome) and rename('.build/main.pdf', ".build/{$nome()}") and print_r($pfx("FINE"));
-    unset($nome);
-    chdir(__DIR__);
-    echo "\n";
-  }
-}
-
-echo "\n";
-
-$dict = __DIR__ . '/.libphp/sweet16-dict';
-echo "Controllo esistenza del dizionario: " . json_encode($e = touch($dict)) . "\n\n";
-$e == false && die(11);
-
-foreach (stream(
-  _find('main.php', __DIR__),
-  _filter(fn ($a) => !str_contains($a, '/Template/')),
-  _map(function ($file) {
-    echo "PREPROCESSING: $file\n";
-    if ((require $file) > 0) {
-      echo "PREPROCESSING: ERRORE A $file\n";
-      die(13);
-    }
-    return compiler();
-  }),
-) as $file) {
-  echo "PREPROCESSING: $file\n";
-  if ((require $file) > 0) {
-    echo "PREPROCESSING: ERRORE A $file\n";
-    die(13);
-  }
-}
-
-echo "\n";
-
-$compile = _compile();
-
-echo implode(
-  "\n",
-  array_map(
-    function ($file) use ($compile) {
-      if ($compile) {
-      }
-    },
-    stream(
-      _find('main.tex', __DIR__),
-      _filter(fn ($a) => !str_contains($a, '/Template/')),
-    )
-  )
-);
-
-main_correttore_ortografico_action($dict, $tex);
-if (_compile()) {
-  main_compile($tex);
 }
