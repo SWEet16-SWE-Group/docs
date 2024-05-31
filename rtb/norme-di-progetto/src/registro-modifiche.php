521dd5cef4e9f3e9d715974542c641da65172aeb
bzn0u95quudr7jutfjap <114179940+bzn0u95quudr7jutfjap@users.noreply.github.com>
2024-05-31 23:07:04 +0200

    spostati i registri nel main

--- rtb/norme-di-progetto/src/registro-modifiche.php
+++ rtb/norme-di-progetto/src/registro-modifiche.php
@@ -6,20 +6,3 @@ row{even}={bg=lightgray},
 row{1}={bg=black, fg=white}
 }
 <?php 
-$registro = (new RegistroModifiche())
-->log(CE, "2024/02/10", alberto_m(), iulius_s(), "Stesura scheletro")
-->log(CE, "2024/02/13", alberto_m(), alex_s(), "Stesura introduzione")
-->log(CE, "2024/02/15", alberto_m(), alex_s(), "Stesura fornitura")
-->log(CE, "2024/02/18", alberto_m(), alex_s(), "Stesura sviluppo")
-->log(CE, "2024/02/23", alex_s(), alberto_m(), "Stesura documentazione")
-->log(DX, "2024/02/27", alberto_m(), iulius_s(), "Aggiunte norme tipografiche")
-->log(CE, "2024/03/03", alex_s(), alberto_m(), "Stesura codifica")
-->log(CE, "2024/03/07", bilal_em(), alberto_m(), "Stesura gestione configurazione")
-->log(CE, "2024/03/11", alberto_m(), alex_s(), "Stesura progettazione")
-->log(DX, "2024/03/12", alex_s(), alberto_m(), "Modifica struttura UC")
-->log(CE, "2024/03/15", alberto_m(), alex_s(), "Stesura gestione qualità")
-->log(CE, "2024/03/19", alberto_m(), bilal_em(), "Stesura validazione")
-->log(CE, "2024/03/24", bilal_em(), alberto_m(), "Stesura verifica")
-->log(CE, "2024/03/29", alberto_m(), alex_s(), "Stesura gestione dei processi")
-->log(SX, "2024/04/15", alex_s(), "", "Approvazione per il rilascio")
-;
