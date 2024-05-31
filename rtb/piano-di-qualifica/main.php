39825438b1a5a02e7f5a87cb6ec0a11191559c2a
bzn0u95quudr7jutfjap <114179940+bzn0u95quudr7jutfjap@users.noreply.github.com>
2024-05-31 23:03:18 +0200

    spostato il registro nel main

--- rtb/piano-di-qualifica/main.php
+++ rtb/piano-di-qualifica/main.php
@@ -7,6 +7,18 @@ require_once 'Validatore.php';
 require_once 'Membri.php';
 
 $titolo = '/home/lumine/Documenti/unipd/2023-2024-swe/docs/rtb/piano-di-qualifica/main.tex';
+
+$registro = (new RegistroModifiche())
+->log(CE, "2024/02/16", alex_s(), alberto_m(), "Stesura scheletro")
+->log(CE, "2024/03/19", bilal_em(), alberto_m(), "Stesura introduzione")
+->log(CE, "2024/03/22", alberto_c(), alberto_m(), "Stesura test")
+->log(CE, "2024/03/25", giovanni_z(), alex_s(), "Stesura qualità di prodotto")
+->log(CE, "2024/04/02", alberto_c(), alex_s(), "Stesura resoconto delle attività di verifica")
+->log(CE, "2024/04/08", giovanni_z(), alberto_m(), "Stesura qualità di processo")
+->log(DX, "2024/04/10", alberto_c(), alex_s(), "Modifica grafici resoconto attività di verifica")
+->log(SX, "2024/04/15", alex_s(), "", "Approvazione per il rilascio")
+;
+
 $error_flag = 0;
 ob_start();
 ob_start(function ($tex) use ($titolo, &$error_flag) {
