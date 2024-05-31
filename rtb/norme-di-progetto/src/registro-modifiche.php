c7d2af97d5f3bedc783beeaaad74ef3720bb69f8
bzn0u95quudr7jutfjap <114179940+bzn0u95quudr7jutfjap@users.noreply.github.com>
2024-05-31 22:38:22 +0200

    funzione dei membri

--- rtb/norme-di-progetto/src/registro-modifiche.php
+++ rtb/norme-di-progetto/src/registro-modifiche.php
@@ -6,19 +6,19 @@ row{even}={bg=lightgray},
 row{1}={bg=black, fg=white}
 }
 Versione, Data, Autore, Verificatore, Descrizione \\
-1.0.0, 2024/04/15, Alex S., "", Approvazione per il rilascio \\ \hline
-0.12.0, 2024/03/29, Alberto M., Alex S., Stesura gestione dei processi \\ \hline
-0.11.0, 2024/03/24, Bilal E. M., Alberto M., Stesura verifica \\ \hline
-0.10.0, 2024/03/19, Alberto M., Bilal E. M., Stesura validazione \\ \hline
-0.9.0, 2024/03/15, Alberto M., Alex S., Stesura gestione qualità \\ \hline
-0.8.1, 2024/03/12, Alex S., Alberto M., Modifica struttura UC \\ \hline
-0.8.0, 2024/03/11, Alberto M., Alex S., Stesura progettazione \\ \hline
-0.7.0, 2024/03/07, Bilal E. M., Alberto M., Stesura gestione configurazione \\ \hline
-0.6.0, 2024/03/03, Alex S., Alberto M., Stesura codifica \\ \hline
-0.5.1, 2024/02/27, Alberto M., Iulius S., Aggiunte norme tipografiche \\ \hline
-0.5.0, 2024/02/23, Alex S., Alberto M., Stesura documentazione \\ \hline
-0.4.0, 2024/02/18, Alberto M., Alex S., Stesura sviluppo \\ \hline
-0.3.0, 2024/02/15, Alberto M., Alex S., Stesura fornitura \\ \hline
-0.2.0, 2024/02/13, Alberto M., Alex S., Stesura introduzione \\ \hline
-0.1.0, 2024/02/10, Alberto M., Iulius S., Stesura scheletro \\ \hline
+1.0.0, 2024/04/15, alex_s(), "", Approvazione per il rilascio \\ \hline
+0.12.0, 2024/03/29, alberto_m(), alex_s(), Stesura gestione dei processi \\ \hline
+0.11.0, 2024/03/24, bilal_em(), alberto_m(), Stesura verifica \\ \hline
+0.10.0, 2024/03/19, alberto_m(), bilal_em(), Stesura validazione \\ \hline
+0.9.0, 2024/03/15, alberto_m(), alex_s(), Stesura gestione qualità \\ \hline
+0.8.1, 2024/03/12, alex_s(), alberto_m(), Modifica struttura UC \\ \hline
+0.8.0, 2024/03/11, alberto_m(), alex_s(), Stesura progettazione \\ \hline
+0.7.0, 2024/03/07, bilal_em(), alberto_m(), Stesura gestione configurazione \\ \hline
+0.6.0, 2024/03/03, alex_s(), alberto_m(), Stesura codifica \\ \hline
+0.5.1, 2024/02/27, alberto_m(), iulius_s(), Aggiunte norme tipografiche \\ \hline
+0.5.0, 2024/02/23, alex_s(), alberto_m(), Stesura documentazione \\ \hline
+0.4.0, 2024/02/18, alberto_m(), alex_s(), Stesura sviluppo \\ \hline
+0.3.0, 2024/02/15, alberto_m(), alex_s(), Stesura fornitura \\ \hline
+0.2.0, 2024/02/13, alberto_m(), alex_s(), Stesura introduzione \\ \hline
+0.1.0, 2024/02/10, alberto_m(), iulius_s(), Stesura scheletro \\ \hline
 \end{tblr}
--- rtb/piano-di-qualifica/src/registro-modifiche.php
+++ rtb/piano-di-qualifica/src/registro-modifiche.php
@@ -6,12 +6,12 @@ row{even}={bg=lightgray},
 row{1}={bg=black, fg=white}
 }
 Versione, Data, Autore, Verificatore, Descrizione \\ \hline
-1.0.0, 2024/04/15, Alex S., "", Approvazione per il rilascio \\ \hline
-0.6.1, 2024/04/10, Alberto C., Alex S., Modifica grafici resoconto attività di verifica \\ \hline
-0.6.0, 2024/04/08, Giovanni Z., Alberto M., Stesura qualità di processo \\ \hline
-0.5.0, 2024/04/02, Alberto C., Alex S., Stesura resoconto delle attività di verifica \\ \hline
-0.4.0, 2024/03/25, Giovanni Z., Alex S., Stesura qualità di prodotto \\ \hline
-0.3.0, 2024/03/22, Alberto C., Alberto M., Stesura test \\ \hline
-0.2.0, 2024/03/19, Bilal E. M., Alberto M., Stesura introduzione \\ \hline
-0.1.0, 2024/02/16, Alex S., Alberto M., Stesura scheletro \\ \hline
+1.0.0, 2024/04/15, alex_s(), "", Approvazione per il rilascio \\ \hline
+0.6.1, 2024/04/10, alberto_c(), alex_s(), Modifica grafici resoconto attività di verifica \\ \hline
+0.6.0, 2024/04/08, giovanni_z(), alberto_m(), Stesura qualità di processo \\ \hline
+0.5.0, 2024/04/02, alberto_c(), alex_s(), Stesura resoconto delle attività di verifica \\ \hline
+0.4.0, 2024/03/25, giovanni_z(), alex_s(), Stesura qualità di prodotto \\ \hline
+0.3.0, 2024/03/22, alberto_c(), alberto_m(), Stesura test \\ \hline
+0.2.0, 2024/03/19, bilal_em(), alberto_m(), Stesura introduzione \\ \hline
+0.1.0, 2024/02/16, alex_s(), alberto_m(), Stesura scheletro \\ \hline
 \end{tblr}
