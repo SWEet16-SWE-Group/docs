e6110fdcf4d449b700995c2056994bd00bf92131
bzn0u95quudr7jutfjap <114179940+bzn0u95quudr7jutfjap@users.noreply.github.com>
2024-06-05 12:12:16 +0200

    a

--- app-frontend/src/router.jsx
+++ app-frontend/src/router.jsx
@@ -65,7 +65,7 @@ const router = createBrowserRouter([
             },
             */
              {
-                path: '*',
+                path: '/dashboardcliente',
                 element: <NotFound />
              },
         ]
