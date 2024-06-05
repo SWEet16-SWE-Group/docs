06d1ca7ea6d0de64f31677eb17ce4a15f109090f
CHXSER <elmoutaren@gmail.com>
2024-06-05 15:10:08 +0200

    Aggiunti modelli per prenotazione, pietanze e ingredienti

---
+++ app-backend/app/Models/Pietanza.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Pietanza extends Model
+{
+    use HasFactory;
+    
+    protected $table = "pietanze";
+
+    protected $fillable = ['ristoratore', 'nome'];
+
+    public function ristoratore()
+    {
+        return $this->belongsTo(Ristoratore::class, 'ristoratore');
+    }
+}
