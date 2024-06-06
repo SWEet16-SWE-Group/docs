7b9cd0236385f56f1f96edb1abc0489544475ef9
bzn0u95quudr7jutfjap <114179940+bzn0u95quudr7jutfjap@users.noreply.github.com>
2024-06-06 06:30:41 +0200

    correzione stampa

--- app-frontend/src/views/SingolaPrenotazioneCliente.jsx
+++ app-frontend/src/views/SingolaPrenotazioneCliente.jsx
@@ -4,14 +4,16 @@ import axiosClient from '../axios-client.js';
 import { useStateContext } from '../contexts/ContextProvider.jsx';
 
 function Prenotazione(a){
-  const url = (id) => `/dettagliprenotazionecliente/${id}`;
+  const url_p = (id) => `/dettagliprenotazionecliente/${id}`;
+  const url_o = (id) => `/menu/${id}`;
   return (<div key={a.id}>
     <div>{a.orario}</div>
     <div>{a.ristoratore}</div>
     <div>{a.stato}</div>
     <div>{a.numero_inviti}</div>
     <div>{a.divisione_conto}</div>
-    <a href={url(a.id)}>Dettagli</a>
+    <a href={url_p(a.id)}>Esamina pagamento</a>
+    <a href={url_o(a.ristoratore)}>Ordina</a>
   </div>);
 }
 
@@ -23,7 +25,10 @@ export default function ClientePrenotazione() {
 
     const fetchPrenotazioni = () => {
       axiosClient.get(`/prenotazione_c/${id}`).then(
-        data => setPrenotazione(data.data)
+        data => { 
+          setPrenotazione(data.data);
+          console.log(data.data);
+        }
       );
     };
 
@@ -31,7 +36,7 @@ export default function ClientePrenotazione() {
 
     return (
         <div className="container mt-5">
-          {prenotazione && Prenotazione(prenotazione)}
+          {prenotazione && Prenotazione(prenotazione[0])}
         </div>
     );
 }
