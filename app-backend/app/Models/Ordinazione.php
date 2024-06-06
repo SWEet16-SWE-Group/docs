e110309d41e1113022bc7802c4f0443a148e6d64
bzn0u95quudr7jutfjap <114179940+bzn0u95quudr7jutfjap@users.noreply.github.com>
2024-06-06 08:36:39 +0200

    creato il modello dell'ordinazione

--- app-backend/app/Models/Ordinazione.php
+++ app-backend/app/Models/Ordinazione.php
@@ -5,7 +5,22 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Ordinazione extends Model
-{
+class Ordinazione extends Model {
     use HasFactory;
+
+    protected $table = "ordinazioni";
+    protected $fillable = ['invito', 'prenotazione', 'pietanza', 'pagamento'];
+
+    public function invito() {
+        return $this->belongsTo(Invito::class, 'invito');
+    }
+
+    public function prenotazione() {
+        return $this->belongsTo(Prenotazione::class, 'prenotazione');
+    }
+
+    public function pietanza() {
+        return $this->belongsTo(Pietanza::class, 'pietanza');
+    }
+
 }
