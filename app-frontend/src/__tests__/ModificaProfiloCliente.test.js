807bbe54b145271a6f7448091ce3185d0e2ae311
IuliusSignorelli <iulius.signorelli@studenti.unipd.it>
2024-06-08 14:23:02 +0200

    aggiunti test

--- app-frontend/src/__tests__/ModificaProfiloCliente.test.js
+++ app-frontend/src/__tests__/ModificaProfiloCliente.test.js
@@ -1,24 +1,36 @@
-import React from 'react';
+import React , {act} from 'react';
 import axiosClient from '../axios-client';
-import { ContextProvider } from '../contexts/ContextProvider';
+import { ContextProvider , useStateContext} from '../contexts/ContextProvider';
 import ModificaProfiloCliente from '../views/ModificaProfiloCliente';
 import '@testing-library/jest-dom/extend-expect';
-import { waitFor } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
+import { MemoryRouter ,Routes , Route } from 'react-router-dom';
 
 jest.mock('../axios-client');
+jest.mock('../contexts/ContextProvider', () => {
+    const originalModule = jest.requireActual('../contexts/ContextProvider');
+    return {
+        ...originalModule,
+        useStateContext: jest.fn(),
+    };
+});
 
 const renderWithContext = (component) => {
     return render(
       <ContextProvider>
-        <MemoryRouter>
-            {component}
+        <MemoryRouter initialEntries={['/modificaprofilocliente/1']}>
+            <Routes>
+                <Route path="/modificaprofilocliente/:id" element={component} />
+            </Routes>
         </MemoryRouter>
       </ContextProvider>
     );
 };
 
 describe('ModificaProfiloCliente', () => {
+
+    let mockUseStateContext;
+    const client_id = "1";
 
     beforeEach( () => {
         axiosClient.get.mockResolvedValueOnce({
@@ -27,6 +39,13 @@ describe('ModificaProfiloCliente', () => {
             }
         }
         );
+        localStorage.setItem('USER_ID',1);
+        mockUseStateContext = {
+            role: 'CLIENT',
+            setNotification: jest.fn(),
+            setNotificationStatus: jest.fn(),
+        };
+        useStateContext.mockReturnValue(mockUseStateContext);
     });
 
     it('Form correctly rendered',async () => {
@@ -36,23 +55,72 @@ describe('ModificaProfiloCliente', () => {
         await waitFor(() => {
             expect(screen.getByText('Modifica le informazioni relative a questo profilo')).toBeInTheDocument();
             expect(screen.getByLabelText('Username')).toBeInTheDocument();
-            expect(screen.getByText(/Tullio/i)).toBeInTheDocument();
+            expect(screen.getByRole('nameChanger',{'value': 'Tullio'})).toBeInTheDocument();
             expect(screen.getByText('Conferma modifiche')).toBeInTheDocument();
             expect(screen.getByText('Annulla')).toBeInTheDocument();
         });
+    });
 
         it('Changed username rendered correctly',async () => {
             renderWithContext(<ModificaProfiloCliente/>);
 
             await waitFor ( () => {
-                expect(screen.getByText(/Tullio/i)).toBeInTheDocument();
+            expect(screen.getByRole('nameChanger',{'value': 'Tullio'})).toBeInTheDocument();
             });
 
             act(() => {
                 fireEvent.change(screen.getByLabelText('Username'), { target: { value: 'Tullio 2:la vendetta' } });
             });
 
-            expect(screen.getByText(/Tullio 2:la vendetta/i)).toBeInTheDocument();
+            expect(screen.getByRole('nameChanger',{'value': 'Tullio 2:la vendetta'})).toBeInTheDocument();
         });
+
+        it('Form submission handled successfully',async () => {
+
+            axiosClient.put.mockResolvedValueOnce({ data: { status: 'success', notification: 'Dati aggiornati con successo.' } });
+
+            renderWithContext(<ModificaProfiloCliente />);
+    
+            await waitFor(() => {
+                expect(screen.getByLabelText('Username').value).toBe('Tullio');
+            });
+
+            act(() => {
+              fireEvent.change(screen.getByLabelText('Username'), { target: { value: 'Tullio 2:la vendetta' } });
+              fireEvent.click(screen.getByText('Conferma modifiche'));
+            });
+    
+            await waitFor(() => {
+                expect(axiosClient.put).toHaveBeenCalledWith('/client', {
+                  //  user: localStorage.getItem('USER_ID'),
+                    id: client_id,
+                    user:localStorage.getItem('USER_ID'),
+                    nome: 'Tullio 2:la vendetta',
+                    role: 'CLIENT',
+                });
+            });
+        });
+
+        it('Form submission goes wrong',async () => {
+            axiosClient.put.mockRejectedValueOnce({ response: { data: { errors: { error : [ "Errore nell'aggiornamento del cliente!"]}
+         } } } );
+    
+            renderWithContext(<ModificaProfiloCliente />);
+    
+            await waitFor(() => {
+                expect(screen.getByRole('nameChanger',{'value': 'Tullio'})).toBeInTheDocument();
+            });
+    
+            act(() => {
+              fireEvent.change(screen.getByLabelText('Username'), { target: { value: 'Tullio 2:il tuo nome non verrà salvato' } });
+              fireEvent.click(screen.getByText('Conferma modifiche'));
+            });
+    
+            await waitFor(() => {
+                expect(screen.getByText('Errore nell\'aggiornamento del cliente!')).toBeInTheDocument();
+            });
+        });
+
+        
     });
-});
+
--- app-frontend/src/views/ModificaProfiloCliente.jsx
+++ app-frontend/src/views/ModificaProfiloCliente.jsx
@@ -1,9 +1,10 @@
 import {useParams, useNavigate, Link} from "react-router-dom";
-import React, {useState , useEffect } from 'react';
+import React, {useState , useEffect }  from 'react';
 import { fetchClientProfile , deleteClientProfile , updateClientProfile } from '../services/ClientService';
 import {useStateContext} from "../contexts/ContextProvider";
 import axios from 'axios';
 import axiosClient from "../axios-client.js";
+import { act } from "react";
 
 export default function ModificaProfiloCliente() {
 
@@ -77,7 +78,7 @@ export default function ModificaProfiloCliente() {
                         <div class="form-group row">
                             <label for="nome" class="col-sm-2 col-form-label">Username</label>
                             <div class="col-sm-10">
-                                <input type="text" class="form-control" id="nome" name="nome" value={username}
+                                <input type="text" class="form-control" id="nome" name="nome" value={username} role="nameChanger"
                                        onChange={ev =>
                                            setUsername(ev.target.value)}
                                 required
