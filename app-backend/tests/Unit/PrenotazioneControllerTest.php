df2c299c59aef4d0bcf3ad9c7356fbeefff84e22
bzn0u95quudr7jutfjap <114179940+bzn0u95quudr7jutfjap@users.noreply.github.com>
2024-06-11 19:53:03 +0200

    tutti i test GET

--- app-backend/tests/Unit/PrenotazioneControllerTest.php
+++ app-backend/tests/Unit/PrenotazioneControllerTest.php
@@ -20,6 +20,39 @@ class PrenotazioneControllerTest extends TestCase
     use RefreshDatabase;
 
     /**
+     * Prenotazione show
+     *
+     * @return void
+     */
+    public function test_show()
+    {
+        (new DatabaseSeeder())->run();
+
+        Sanctum::actingAs(User::where('id', 1)->first());
+        $prenotazione = (object)['id' => 1];
+        $response = $this->getJson('/api/prenotazioni/' . $prenotazione->id);
+
+        $response->assertStatus(200);
+        $response->assertJsonFragment(['id' => 1]);
+    }
+
+    /**
+     * Prenotazione dashboard_c
+     *
+     * @return void
+     */
+    public function test_dashboard_c()
+    {
+        (new DatabaseSeeder())->run();
+
+        Sanctum::actingAs(User::where('id', 1)->first());
+        $prenotazione = (object)['id' => 1];
+        $response = $this->getJson('/api/dashboard_c/' . $prenotazione->id);
+
+        $response->assertStatus(200);
+    }
+
+    /**
      * Prenotazione cliente
      *
      * @return void
@@ -47,6 +80,54 @@ class PrenotazioneControllerTest extends TestCase
         Sanctum::actingAs(User::where('id', 1)->first());
         $prenotazione = (object)['id' => 1];
         $response = $this->getJson('/api/prenotazione_conto/' . $prenotazione->id);
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     * Prenotazione dettagli
+     *
+     * @return void
+     */
+    public function test_prenotazione_dettagli()
+    {
+        (new DatabaseSeeder())->run();
+
+        Sanctum::actingAs(User::where('id', 1)->first());
+        $prenotazione = (object)['id' => 1];
+        $response = $this->getJson('/api/prenotazione_dettagli/' . $prenotazione->id);
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     * Prenotazione pagamenti ordinazioni
+     *
+     * @return void
+     */
+    public function test_prenotazione_pagamenti_ordinazioni()
+    {
+        (new DatabaseSeeder())->run();
+
+        Sanctum::actingAs(User::where('id', 1)->first());
+        $prenotazione = (object)['id' => 1];
+        $response = $this->getJson('/api/pagamenti_ordinazioni/' . $prenotazione->id);
+
+        $response->assertStatus(200);
+    }
+
+    /**
+     * Prenotazione pagamenti inviti
+     *
+     * @return void
+     */
+    public function test_prenotazione_pagamenti_inviti()
+    {
+        (new DatabaseSeeder())->run();
+
+        Sanctum::actingAs(User::where('id', 1)->first());
+        $prenotazione = (object)['id' => 1];
+        $response = $this->getJson('/api/pagamenti_inviti/' . $prenotazione->id);
 
         $response->assertStatus(200);
     }
