d466b78842a5d9aa557034bdc4000dc82745897a
bzn0u95quudr7jutfjap <114179940+bzn0u95quudr7jutfjap@users.noreply.github.com>
2024-06-11 21:34:43 +0200

    ordinazioni test

--- app-backend/tests/Unit/OrdinazioniControllerTest.php
+++ app-backend/tests/Unit/OrdinazioniControllerTest.php
@@ -2,17 +2,52 @@
 
 namespace Tests\Unit;
 
-use PHPUnit\Framework\TestCase;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\User;
+use DatabaseSeeder;
+use Tests\TestCase;
+use Laravel\Sanctum\Sanctum;
+
+require_once __DIR__ . '/../../database/seeds/DatabaseSeeder.php' ;
 
 class OrdinazioniTest extends TestCase
 {
+
+
+    use RefreshDatabase;
     /**
-     * A basic unit test example.
+     * crea
      *
      * @return void
      */
-    public function test_example()
+    public function test_crea()
     {
-        $this->assertTrue(true);
+        (new DatabaseSeeder())->run();
+        Sanctum::actingAs(User::where('id', 1)->first());
+
+        $data = [
+            'invito' => 1,
+            'pietanza' => 2,
+        ];
+        $response = $this->post('/api/crea-ordinazione/',$data);
+
+        $response->assertStatus(201);
+        $this->assertDatabaseHas('ordinazioni',$data);
+    }
+
+    /**
+     * paga
+     *
+     * @return void
+     */
+    public function test_paga()
+    {
+        (new DatabaseSeeder())->run();
+        Sanctum::actingAs(User::where('id', 1)->first());
+
+        $response = $this->post('/api/paga_ordinazione/1');
+
+        $response->assertStatus(200);
+        $this->assertDatabaseHas('ordinazioni',['id' => 1, 'pagamento' => 'PAGATO',]);
     }
 }
