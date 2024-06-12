82cdb452c8ac0c5b51331d595d134eeabad84cce
albyx97 <148105963+albyx97@users.noreply.github.com>
2024-06-12 11:01:27 +0200

    Creati design pattern utilizzati nel front-end

--- pb/specifica-tecnica/src/architettura.php
+++ pb/specifica-tecnica/src/architettura.php
@@ -13,3 +13,19 @@ Inoltre, sfrutta gli hooks forniti da React, come useState e useEffect, per gest
 Grazie a questo approccio, l'applicazione è in grado di fornire un'esperienza utente fluida e reattiva, adattandosi in tempo reale alle esigenze dell'utente. 
 Le singole chiamate alle API permettono di recuperare i dati in modo efficiente, sicuro e scalabile, separando la logica di business dall’interfaccia utente. 
 L'uso degli hook consente di gestire lo stato dell’applicazione in modo dichiarativo e modulare, semplificando la leggibilità del codice e la gestione dei dati.
+
+\subsubsection{Diagrammi delle classi}
+
+DA FARE UNA VOLTA TERMINATA LA CODIFICA
+
+\subsubsection{Design Pattern utilizzati}
+
+In questa sezione, vengono descritti i design pattern utilizzati nell’applicazione basata su React. Sono state adottate diverse soluzioni per modularizzare le singole componenti e adattarle alle specifiche esigenze di realizzazione ed integrazione previste. \\
+In particolare, possiamo dettagliare l’utilizzo di:
+\begin{itemize}
+\item \textbf{React Hook}: utilizzati per gestire lo stato dell’applicazione in modo efficiente e visualizzare dinamicamente le informazioni. 
+Vengono impiegati sia gli hook nativi di React (come useState, useEffect e useContext), sia hook personalizzati creati in base alle esigenze delle singole viste e componenti, come precedentemente descritto.
+\item \textbf{Conditional Rendering}: permette di mostrare contenuti diversi in base a determinate condizioni. Vengono sviluppati componenti in grado di verificare suddette condizioni e rendere visibili i dati pertinenti in base a queste.
+\item \textbf{Compound Components}: consentono di modularizzare le singole componenti attraverso una gerarchia padre-figlio, dove un componente padre contiene uno o più componenti figlio. 
+Questo approccio permette di specializzare la gestione dei dati e personalizzare l’interfaccia utente in modo centralizzato, seguendo una lista di opzioni unica.
+\end{itemize}
