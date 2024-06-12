a9590771b2b11fb3d42f46a10a7f3a320746da21
bzn0u95quudr7jutfjap <114179940+bzn0u95quudr7jutfjap@users.noreply.github.com>
2024-06-12 14:16:36 +0200

    scheletro clientcontrollertest

---
+++ app-backend/tests/Unit/ClientControllerTest.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+use Laravel\Sanctum\Sanctum;
+use DatabaseSeeder;
+
+require_once __DIR__ . '/../../database/seeds/DatabaseSeeder.php' ;
+
+
+class ClientControllerTest extends TestCase
+{
+
+    use RefreshDatabase;
+
+    /**
+     * show
+     *
+     * @return void
+     */
+    public function test_show()
+    {
+        $this->assertTrue(true);
+    }
+
+    /**
+     * store
+     *
+     * @return void
+     */
+    public function test_store()
+    {
+        $this->assertTrue(true);
+    }
+
+    /**
+     * index
+     *
+     * @return void
+     */
+    public function test_index()
+    {
+        $this->assertTrue(true);
+    }
+
+    /**
+     * update
+     *
+     * @return void
+     */
+    public function test_update()
+    {
+        $this->assertTrue(true);
+    }
+
+    /**
+     * destroy
+     *
+     * @return void
+     */
+    public function test_destroy()
+    {
+        $this->assertTrue(true);
+    }
+}
