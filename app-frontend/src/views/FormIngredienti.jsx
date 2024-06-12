a527065daaa9fe3c451524476d604559cbfb589a
Alberto Michelazzo <alby.michelazzo@gmail.com>
2024-06-12 16:18:07 +0200

    aggiunta parte grafica nel frontend

--- app-frontend/src/views/FormIngredienti.jsx
+++ app-frontend/src/views/FormIngredienti.jsx
@@ -1,7 +1,8 @@
-import { useState } from "react";
+import React, {useEffect, useState} from "react";
 import axiosClient from "../axios-client";
 import { useNavigate, useParams } from "react-router-dom";
 import { useStateContext } from "../contexts/ContextProvider";
+import { fetchAllergeni } from "../services/IntolleranzeService";
 
 export default function FormIngrediente() {
     const { ristoratoreId } = useParams();
@@ -9,6 +10,33 @@ export default function FormIngrediente() {
     const { setNotification, setNotificationStatus } = useStateContext();
     const [nome, setNome] = useState('');
     const [errorMessage, setErrorMessage] = useState('');
+    const [allergeni,setAllergeni] = useState([]);
+
+    //selectedIds rappresenta gli allergeni selezionati
+    const [selectedIds, setSelectedIds] = useState([]);
+
+    const handleCheckboxChange = (event) => {
+        const checkedId = event.target.value;
+        if(event.target.checked){
+            setSelectedIds([...selectedIds,checkedId])
+        }else{
+            setSelectedIds(selectedIds.filter(id=>id !== checkedId))
+        }
+    }
+
+    async function getAllergeni() {
+        try {
+            const allergeni = await fetchAllergeni();
+            setAllergeni(allergeni);
+        } catch (error) {
+            console.error('Error fetching allergeni:', error);
+        }
+    }
+
+    useEffect( () => {
+        getAllergeni();
+    },[]);
+
 
     const handleSubmit = async (e) => {
         e.preventDefault();
@@ -17,7 +45,8 @@ export default function FormIngrediente() {
         try {
             const formData = {
                 ristoratore: ristoratoreId,
-                nome: nome
+                nome: nome,
+                allergie: selectedIds
             };
             await axiosClient.post('/ingredienti', formData);
 
@@ -50,8 +79,34 @@ export default function FormIngrediente() {
                         required
                     />
                 </div>
-                <button type="submit" className="btn btn-primary">Aggiungi</button>
-                <button type="button" className="btn btn-secondary ms-2" onClick={() => navigate(`/gestioneingredienti/${ristoratoreId}`)}>Annulla</button>
+                {allergeni.length === 0 ? (<p>Loading...</p>) : (
+                    <div>
+                        <p>Seleziona uno o più allergeni contenuti in questo ingrediente: </p>
+                        <br />
+                        {allergeni.map((allergene) => {
+                            return (
+                                <div className="form-check flex">
+                                    <input
+                                        className="form-check-input"
+                                        type="checkbox"
+                                        value={allergene.id}
+                                        id={allergene.id}
+                                        onChange={(event) => {
+                                            handleCheckboxChange(event)
+                                        }
+                                        }/>
+                                    <label className="form-check-label" for={allergene.id}>
+                                        {allergene.nome}
+                                    </label>
+                                </div>
+                            );
+                        })}
+                    </div>)}
+                <br />
+                <div>
+                    <button type="submit" className="btn btn-primary">Aggiungi</button>
+                    <button type="button" className="btn btn-secondary ms-2" onClick={() => navigate(`/gestioneingredienti/${ristoratoreId}`)}>Annulla</button>
+                </div>
             </form>
         </div>
     );
