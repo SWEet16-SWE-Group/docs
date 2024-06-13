813a204b3242f6c2cb44f88b0e5902eee2afb414
bzn0u95quudr7jutfjap <114179940+bzn0u95quudr7jutfjap@users.noreply.github.com>
2024-06-13 17:03:29 +0200

    tabella notifiche

---
+++ app-backend/database/migrations/2024_06_13_144745_create_notifiche_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNotificheTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('notifiche', function (Blueprint $table) {
+            $table->id();
+            $table->enum('lettura', ['NON LETTO','LETTO'])->default('NON LETTO');
+            $table->foreignId('prenotazione')->nullable()->constrained('prenotazioni')->onDelete('cascade');
+            $table->foreignId('ordinazione')->nullable()->constrained('ordinazioni')->onDelete('cascade');
+            $table->foreignId('inviti')->nullable()->constrained('inviti')->onDelete('cascade');
+            $table->enum('significato', [
+                'PRENOTAZIONE CREATA',
+                'PRENOTAZIONE ACCETTATA',
+                'PRENOTAZIONE RIFIUTATA',
+                'PRENOTAZIONE CONTO',
+                'PRENOTAZIONE PAGAMENTO',
+                'INVITO',
+                'ORDINAZIONE CREATA',
+                'ORDINAZIONE CANCELLATA',
+            ]);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('notifiche');
+    }
+}
