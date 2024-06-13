15040538c813445c9c7ee81fee8e4f24276796d5
Alberto Michelazzo <alby.michelazzo@gmail.com>
2024-06-13 20:56:21 +0200

    eliminazione ordinazioni

--- app-frontend/src/views/SingolaPrenotazioneCliente.jsx
+++ app-frontend/src/views/SingolaPrenotazioneCliente.jsx
@@ -1,55 +1,7 @@
 import React, { useEffect, useState } from "react";
 import {Link, useNavigate, useParams} from 'react-router-dom';
 import axiosClient from '../axios-client.js';
-import { useStateContext } from '../contexts/ContextProvider.jsx';
-
-function Ordinazioni({data}){
-  return (
-    <>
-      <thead>
-        <tr><th colSpan="4"><h3>{data.nome}</h3></th></tr>
-        <tr>
-          <th>Pietanza</th>
-          <th>Aggiunte</th>
-          <th>Rimozioni</th>
-          <th>Azioni</th>
-        </tr>
-      </thead>
-      <tbody>
-        {data.ordinazioni.map(ordinazione => (
-          <tr key={ordinazione.id}>
-              <td>{ordinazione.pietanza}</td>
-              <td>{ordinazione.aggiunte}</td>
-              <td>{ordinazione.rimozioni}</td>
-              <td><button className="btn btn-block">Cancella</button></td>
-          </tr>
-        ))}
-      </tbody>
-    </>
-  );
-}
-
-function Prenotazione(p){
-  const url_p = (id) => `/divisionecontopagamentocliente/${id}`;
-  const url_o = (r,p) => `/menu/${r}/${p}`;
-  const a = p.prenotazione;
-
-return (<div key={a.id}>
-    <h1>{a.nome}</h1>
-    <h2>Dettagli</h2>
-    <div>Codice di invito: <Link>{a.id}</Link></div>
-    <div>Stato: {a.stato}</div>
-    <div>Orario: {a.orario}</div>
-    <div><Link to={url_p(a.id)}>Esamina pagamento</Link></div>
-    {a && a.stato === 'Accettata' &&
-        <div><Link to={url_o(a.ristoratore, a.id)}>Ordina</Link></div>
-    }
-    <h2>Ordinazioni</h2>
-    <table className="table">
-        {p.ordinazioni && p.ordinazioni.map((data) => <Ordinazioni key={data.nome} data={data}/>)}
-    </table>
-</div>);
-}
+import {useStateContext} from '../contexts/ContextProvider.jsx';
 
 export default function ClientePrenotazione() {
 
@@ -69,21 +21,87 @@ export default function ClientePrenotazione() {
 
     useEffect(fetchPrenotazioni, []);
 
+    const Prenotazione = (p) => {
+        const url_p = (id) => `/divisionecontopagamentocliente/${id}`;
+        const url_o = (r,p) => `/menu/${r}/${p}`;
+        const a = p.prenotazione;
+
+        return (<div key={a.id}>
+            <h1>{a.nome}</h1>
+            <h2>Dettagli</h2>
+            <div>Codice di invito: <Link>{a.id}</Link></div>
+            <div>Stato: {a.stato}</div>
+            <div>Orario: {a.orario}</div>
+            <div><Link to={url_p(a.id)}>Esamina pagamento</Link></div>
+            {a && a.stato === 'Accettata' &&
+                <div><Link to={url_o(a.ristoratore, a.id)}>Ordina</Link></div>
+            }
+            {p.ordinazioni.length !== 0 &&
+            <>
+            <h2>Ordinazioni</h2>
+            <table className="table">
+                {p.ordinazioni && p.ordinazioni.map((data) => <Ordinazioni key={data.nome} data={data}/>)}
+            </table>
+            </>
+        }
+        </div>);
+    }
+
+    const Ordinazioni = ({data}) => {
+        return (
+            <>
+                <thead>
+                <tr><th colSpan="4"><h3>{data.nome}</h3></th></tr>
+                <tr>
+                    <th>Pietanza</th>
+                    <th>Aggiunte</th>
+                    <th>Rimozioni</th>
+                    <th>Azioni</th>
+                </tr>
+                </thead>
+                <tbody>
+                {data.ordinazioni.map(ordinazione => (
+                    <tr key={ordinazione.id}>
+                        <td>{ordinazione.pietanza}</td>
+                        <td>{ordinazione.aggiunte}</td>
+                        <td>{ordinazione.rimozioni}</td>
+                        <td><button onClick={() => deleteOrdinazione(ordinazione.id)} className="btn btn-block">Cancella</button></td>
+                    </tr>
+                ))}
+                </tbody>
+            </>
+        );
+    }
     const deletePrenotazione = async () => {
 
-        if (!window.confirm("Sei sicuro di voler annullare questa prenotazione?")) {
+        if (!window.confirm("Sei sicuro di voler annullare questa prenotazione?"))
             return;
-        }
 
         try {
             const response = await axiosClient.delete(`/prenotazione/${id}`);
             setNotificationStatus('success');
             setNotification('Prenotazione annullata con successo.');
             navigate('/dashboardcliente');
-
         } catch (error) {
             setNotificationStatus('error');
             setNotification('Errore durante l\'eliminazione della prenotazione.');
+        }
+    }
+
+    const deleteOrdinazione = async (id) => {
+
+        if (!window.confirm("Sei sicuro di voler eliminare questa ordinazione?"))
+            return;
+
+        try {
+            const response = await axiosClient.delete(`/ordinazione/${id}`);
+            setNotificationStatus('success');
+            setNotification('Ordinazione eliminata con successo.');
+
+            window.location.reload();
+        } catch (error) {
+            setNotificationStatus('error');
+            setNotification('Errore durante l\'eliminazione dell\'ordinazione.');
         }
     }
 
