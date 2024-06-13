8a5cea27342c22f688867004a64f17e2b300d746
Alberto Michelazzo <alby.michelazzo@gmail.com>
2024-06-13 20:00:56 +0200

    aggiunta annulla prenotazione

--- app-frontend/src/views/SingolaPrenotazioneCliente.jsx
+++ app-frontend/src/views/SingolaPrenotazioneCliente.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { Link, useParams } from 'react-router-dom';
+import {Link, useNavigate, useParams} from 'react-router-dom';
 import axiosClient from '../axios-client.js';
 import { useStateContext } from '../contexts/ContextProvider.jsx';
 
@@ -33,29 +33,30 @@ function Prenotazione(p){
   const url_p = (id) => `/divisionecontopagamentocliente/${id}`;
   const url_o = (r,p) => `/menu/${r}/${p}`;
   const a = p.prenotazione;
-    console.log(a.id);
-  return (<div key={a.id}>
+
+return (<div key={a.id}>
     <h1>{a.nome}</h1>
     <h2>Dettagli</h2>
-      <div>Codice di invito: <Link>{a.id}</Link></div>
-      <div>Stato: {a.stato}</div>
-      <div>Orario: {a.orario}</div>
+    <div>Codice di invito: <Link>{a.id}</Link></div>
+    <div>Stato: {a.stato}</div>
+    <div>Orario: {a.orario}</div>
     <div><Link to={url_p(a.id)}>Esamina pagamento</Link></div>
-      {a && a.stato === 'Accettata'&&
-        <div><Link to={url_o(a.ristoratore,a.id)}>Ordina</Link></div>
-      }
+    {a && a.stato === 'Accettata' &&
+        <div><Link to={url_o(a.ristoratore, a.id)}>Ordina</Link></div>
+    }
     <h2>Ordinazioni</h2>
-      <table className="table">
-      {p.ordinazioni.map((data) => <Ordinazioni key={data.nome} data={data}/>)}
-      </table>
-  </div>);
+    <table className="table">
+        {p.ordinazioni && p.ordinazioni.map((data) => <Ordinazioni key={data.nome} data={data}/>)}
+    </table>
+</div>);
 }
 
 export default function ClientePrenotazione() {
 
-    const {user, profile, token, role, notification, notificationStatus, setUser, setToken, setRole} = useStateContext()
+    const {user, profile, token, role, notification, notificationStatus, setUser, setToken, setRole,setNotificationStatus,setNotification} = useStateContext()
     const {id} = useParams();
     const [prenotazione, setPrenotazione] = useState(null);
+    const navigate = useNavigate();
 
     const fetchPrenotazioni = () => {
       axiosClient.get(`/prenotazione_c/${id}`).then(
@@ -68,13 +69,33 @@ export default function ClientePrenotazione() {
 
     useEffect(fetchPrenotazioni, []);
 
+    const deletePrenotazione = async () => {
+
+        if (!window.confirm("Sei sicuro di voler annullare questa prenotazione?")) {
+            return;
+        }
+
+        try {
+            const response = await axiosClient.delete(`/prenotazione/${id}`);
+            setNotificationStatus('success');
+            setNotification('Prenotazione annullata con successo.');
+            navigate('/dashboardcliente');
+
+        } catch (error) {
+            setNotificationStatus('error');
+            setNotification('Errore durante l\'eliminazione della prenotazione.');
+        }
+    }
+
     return (
         <>
             <div className="container mt-5">
               {prenotazione && Prenotazione(prenotazione)}
             </div>
             <div>
-                <Link to='/dashboardcliente' className='btn btn-primary'>Annulla</Link>
+                <Link to='/dashboardcliente' className='btn btn-primary'>Indietro</Link>
+                &nbsp; &nbsp;
+                <button onClick={deletePrenotazione} className='btn btn-primary'>Annulla prenotazione</button>
             </div>
         </>
     );
