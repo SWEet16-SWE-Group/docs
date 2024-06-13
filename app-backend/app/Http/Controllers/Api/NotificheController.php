99c0ffb34c3982898e53739b1519f477888930ce
bzn0u95quudr7jutfjap <114179940+bzn0u95quudr7jutfjap@users.noreply.github.com>
2024-06-13 18:53:34 +0200

    implementato il conto e l'aggiornamento

--- app-backend/app/Http/Controllers/Api/NotificheController.php
+++ app-backend/app/Http/Controllers/Api/NotificheController.php
@@ -5,15 +5,26 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use DB;
+
 class NotificheController extends Controller
 {
+    private $q = [
+        'cliente' => <<<'EOF'
+        EOF,
+        'ristoratore' => <<<'EOF'
+        EOF,
+    ];
     public function notifiche($id,$tipo){
-        $return = [];
+        $return = DB::select($this->q[$tipo]);
+        foreach ($return as $a) {
+            DB::update('update notifiche as n set n.lettura = "LETTO" where n.id = ? ;',[$a]);
+        }
         return response()->json($return, 200);
     }
 
     public function count($id,$tipo){
-        $return = [];
+        $return = DB::select("select count(*) as c from ({$this->q[$tipo]})",[$id])[0]['c'];
         return response()->json($return, 200);
     }
 }
