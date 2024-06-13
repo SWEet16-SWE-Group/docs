e4f47a0142b2ac3ed45e74e50173150c2659d44f
bzn0u95quudr7jutfjap <114179940+bzn0u95quudr7jutfjap@users.noreply.github.com>
2024-06-13 21:17:13 +0200

    possibile soluzione alle notifiche

--- app-backend/app/Http/Controllers/Api/NotificheController.php
+++ app-backend/app/Http/Controllers/Api/NotificheController.php
@@ -9,10 +9,47 @@ use DB;
 
 class NotificheController extends Controller
 {
+
+    private $prenotazioni_ristoratori_cliente = <<<'EOF'
+    select
+        p.id as prenotazione_id,
+        p.orario,
+        p.numero_inviti,
+        p.divisione_conto,
+        p.stato,
+        r.id as ristoratore_id,
+        r.nome as ristorante_nome,
+        r.cucina,
+        r.indirizzo,
+        r.telefono,
+        r.capienza,
+        c.id as cliente_id,
+        c.nome as cliente_nome
+    from prenotazioni as p
+    inner join ristoratori as r on r.id = p.ristoratore
+    inner join inviti as i on p.id = i.prenotazione
+    inner join clients as c on c.id = i.cliente
+    inner join (
+        select p.id, min(i.created_at) as min
+        from prenotazioni as p
+        inner join inviti as i on p.id = i.prenotazione
+        group by p.id
+    ) as imin on i.created_at = imin.min
+    EOF;
+
     private $q = [
         'cliente' => <<<'EOF'
         EOF,
         'ristoratore' => <<<'EOF'
+        select
+            r.id as r_id,
+            n.significato,
+            n.lettura,
+        from notifiche as n
+        left join (PRENOTAZIONIRISTORATORICLIENTE) as p
+            on p.prenotazione_id = n.prenotazione
+            and n.significato = 'PRENOTAZIONE CREATA'
+        left join () as
         EOF,
     ];
     public function notifiche($id,$tipo){
