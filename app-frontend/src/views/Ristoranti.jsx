55d48366db99eaeb77654715cfeb4d4c29a276b9
bzn0u95quudr7jutfjap <114179940+bzn0u95quudr7jutfjap@users.noreply.github.com>
2024-06-13 11:28:17 +0200

    filtri primordiali

--- app-frontend/src/views/Ristoranti.jsx
+++ app-frontend/src/views/Ristoranti.jsx
@@ -4,30 +4,45 @@ import axiosClient from "../axios-client.js";
 import {createRef} from "react";
 import {useStateContext} from "../contexts/ContextProvider.jsx";
 
-function ristoranti(set) {
-  axiosClient.get('/ristoranti').then(
-    data => { set(data.data) }
-  )
-}
-
-function url(id){
-  return `/ristorante/${id}`;
-}
-
 function ristorante(a){
   return (
-      <div>
-        <Link to={url(a.id)} key={a.id}> {a.nome} @ {a.indirizzo} # {a.telefono} | {a.orario} </Link>
+      <div key={a.id}>
+        <Link to={`/ristorante/${a.id}`} key={a.id}> {a.nome} @ {a.indirizzo} # {a.telefono} | {a.orario} </Link>
       </div>
   );
 }
 
 export default function Ristoranti() {
-    const [r,sr] = useState(null);
-    useEffect(() => ristoranti(sr) ,[]);
+    const [ristorantiTutti,setRistorantiTutti] = useState(null);
+    const [ristoranti,setRistoranti] = useState(null);
+    const [nome,setNome] = useState(null);
+    const [cucina,setCucina] = useState(null);
+    const [orario,setOrario] = useState(null);
+
+    function applicafiltri(){
+      setRistoranti(
+         ristorantiTutti
+          //.filter((r) => r.nome.contains(nome) && (cucina === null || r.cucina === cucina))
+      );
+      debugger;
+    }
+
+    async function fetch(){
+      const {data: data} = await axiosClient.get('/ristoranti');
+      setRistorantiTutti(data);
+      applicafiltri();
+    }
+
+    useEffect(() => { fetch(); }, []);
+
+    console.log('========================');
+    console.log(ristorantiTutti);
+    console.log(ristoranti);
+    console.log('------------------------');
+
     return (
         <div>
-            {r ? r.map(ristorante) : <div>Nessun ristorante ancora registrato</div>}
+            {ristoranti ? ristoranti.map(ristorante) : <div>Nessun ristorante corrisponde ai filtri scelti</div>}
         </div>
     )
 }
