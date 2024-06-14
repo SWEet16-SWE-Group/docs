49c824ec2911c8b0ff22cc238a68089175ae419c
bzn0u95quudr7jutfjap <114179940+bzn0u95quudr7jutfjap@users.noreply.github.com>
2024-06-14 11:29:19 +0200

    costruzione grafica delle notifiche

--- app-frontend/src/views/Notifiche.jsx
+++ app-frontend/src/views/Notifiche.jsx
@@ -6,13 +6,41 @@ import {useStateContext} from "../contexts/ContextProvider.jsx";
 import RestaurantCard from "../components/RestaurantCard.jsx";
 
 function riga(a){
-  a = ({
+  const b = ({
+    'PRENOTAZIONE CREATA':    a => ({
+      desc:<p>{a.cliente.nome} ha creato una <Link to="">prenotazione</Link>.</p>
+    }),
+    'PRENOTAZIONE STATO':     a => ({
+      desc:<p>{a.ristoratore.nome} ha reso tua <Link to="">prenotazione</Link>: {a.prenotazione.stato}.</p>
+    }),
+    'PRENOTAZIONE CONTO':     a => ({
+      desc:<p>{a.cliente.nome} ha scelto una divisione del conto per la <Link to="">prenotazione</Link>.</p>
+    }),
+    'PRENOTAZIONE CANCELLATA':a => ({
+      desc:<p>{a.cliente.nome} ha cancellato la sua prenotazione.</p>
+    }),
+    'INVITO ACCETTATO':       a => ({
+      desc:<p>{a.cliente.nome} ha accettato l'invito alla <Link to="">prenotazione</Link>.</p>
+    }),
+    'INVITO PAGATO':          a => ({
+      desc:<p>{a.cliente.nome} ha pagato la sua <Link to="">quota</Link>.</p>
+    }),
+    'ORDINAZIONE CREATA':     a => ({
+      desc:<p>{a.cliente.nome} ha ordinato: <Link to="">{a.pietanza.nome}</Link>.</p>
+    }),
+    'ORDINAZIONE CANCELLATA': a => ({
+      desc:<p>{a.cliente.nome} ha cancellato la sua ordinazione.</p>
+    }),
+    'ORDINAZIONE PAGATA':     a => ({
+      desc:<p>{a.cliente.nome} ha pagato: <Link to="">{a.pietanza.nome}</Link>.</p>
+    }),
   })[a.significato](a);
-  return <tr><td>{a.time}</td><td>{a.desc}</td></tr>;
+  c = ({time: a.created_at, desc: b.desc});
+  return (<tr key={c.time}><td>{c.time}</td><td>{c.desc}</td></tr>);
 }
 
 export default function Notifiche() {
-    const {role, profile} = useContext();
+    const {role, profile} = useStateContext();
     const [notifiche, setNotifiche] = useState(null);
 
     async function fetch(){
