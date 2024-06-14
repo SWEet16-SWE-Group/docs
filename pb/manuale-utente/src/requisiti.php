27ded06e22a9742ce01bbe4bdae7b93ddb352325
albyx97 <148105963+albyx97@users.noreply.github.com>
2024-06-14 10:38:53 +0200

    Creato requisiti hardware

--- pb/manuale-utente/src/requisiti.php
+++ pb/manuale-utente/src/requisiti.php
@@ -1,1 +1,43 @@
+\pagebreak
+
+\section{Requisiti}
+
+In questa sezione sono elencati i requisiti minimi necessari all’esecuzione dell’applicazione, elencando le caratteristiche necessarie atte alla configurazione dell’ambiente di sviluppo del progetto. 
+
+\subsection{Requisiti di sistema}
+
+Per far sì che le operazioni di installazione e avvio del prodotto avvengano correttamente e avere un’esperienza soddisfacente e completa nell’uso dell’applicazione, è necessario installare i seguenti software.
+
+\begin{center}
+\begin{longtblr}{
+colspec={|X[m,3cm]|X[m,1.5cm]|X[m,11cm]|},
+row{odd}={bg=white},
+row{even}={bg=lightgray}
+}
+\hline
+\textbf{Componente} & \textbf{Versione} & \textbf{Riferimenti per il download} \\ \hline
+Node.js & \(\ge18.x.x\) & \url{https://nodejs.org/en/} \\ 
+Npm & \(\ge9.0.x\) & Integrato con il download di Node.js \\ 
+Composer & \(\ge1.0\) & \url{https://getcomposer.org/} \\ 
+\end{longtblr}
+\end{center}
+
+\subsection{Requisiti hardware}
+
+L’applicazione esegue su browser, come tale non si individuano dei requisiti specifici, fissati da parte del proponente, del capitolato o dal progetto stesso. I seguenti, pertanto, sono individuati come riferimento di massima per l’esecuzione del prodotto creato.
+
+\begin{center}
+\begin{longtblr}{
+colspec={|X[m,4.5cm]|X[m,11cm]|},
+row{odd}={bg=white},
+row{even}={bg=lightgray}
+}
+\hline
+\textbf{Componente} & \textbf{Requisito} \\ \hline
+Processore & Quad-Core 3,2 GHz \\ 
+Memoria & 8GB DDR4 \\ 
+Scheda grafica & Supporto a scheda grafica integrata con supporto OpenGL \\ 
+Connessione Internet & Connessione Internet stabile e veloce, in grado di supportare le esigenze di traffico dell'applicazione \\ 
+\end{longtblr}
+\end{center}
 
