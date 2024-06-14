09a28418e97ad5429201a893cb82980095dc407a
bzn0u95quudr7jutfjap <114179940+bzn0u95quudr7jutfjap@users.noreply.github.com>
2024-06-14 13:28:30 +0200

    aggiornata la pagina di riproduzione

--- app-frontend/src/views/Notifiche.jsx
+++ app-frontend/src/views/Notifiche.jsx
@@ -5,34 +5,35 @@ import {createRef} from "react";
 import {useStateContext} from "../contexts/ContextProvider.jsx";
 import RestaurantCard from "../components/RestaurantCard.jsx";
 
-function riga(a){
+function Riga(a){
+  const {role} = useStateContext();
   const b = ({
     'PRENOTAZIONE CREATA':    a => ({
-      desc:<p>{a.cliente.nome} ha creato una <Link to="">prenotazione</Link>.</p>
+      desc:<p>{a.d.c_nome} ha creato una <Link to="">prenotazione</Link>.</p>
     }),
     'PRENOTAZIONE STATO':     a => ({
-      desc:<p>{a.ristoratore.nome} ha reso tua <Link to="">prenotazione</Link>: {a.prenotazione.stato}.</p>
+      desc:<p>{a.d.r_nome} ha reso tua <Link to="">prenotazione</Link>: {a.prenotazione.stato}.</p>
     }),
     'PRENOTAZIONE CONTO':     a => ({
-      desc:<p>{a.cliente.nome} ha scelto una divisione del conto per la <Link to="">prenotazione</Link>.</p>
+      desc:<p>{a.d.c_nome} ha scelto una divisione del conto per la <Link to="">prenotazione</Link>.</p>
     }),
     'PRENOTAZIONE CANCELLATA':a => ({
-      desc:<p>{a.cliente.nome} ha cancellato la sua prenotazione.</p>
+      desc:<p>{a.d.c_nome} ha cancellato la sua prenotazione.</p>
     }),
     'INVITO ACCETTATO':       a => ({
-      desc:<p>{a.cliente.nome} ha accettato l'invito alla <Link to="">prenotazione</Link>.</p>
+      desc:<p>{a.d.c_nome} ha accettato l'invito alla <Link to="">prenotazione</Link>.</p>
     }),
     'INVITO PAGATO':          a => ({
-      desc:<p>{a.cliente.nome} ha pagato la sua <Link to="">quota</Link>.</p>
+      desc:<p>{a.d.c_nome} ha pagato la sua <Link to="">quota</Link>.</p>
     }),
     'ORDINAZIONE CREATA':     a => ({
-      desc:<p>{a.cliente.nome} ha ordinato: <Link to="">{a.pietanza.nome}</Link>.</p>
+      desc:<p>{a.d.c_nome} ha ordinato: <Link to="">{a.d.pz_nome}</Link>.</p>
     }),
     'ORDINAZIONE CANCELLATA': a => ({
-      desc:<p>{a.cliente.nome} ha cancellato la sua ordinazione.</p>
+      desc:<p>{a.d.c_nome} ha cancellato la sua ordinazione.</p>
     }),
     'ORDINAZIONE PAGATA':     a => ({
-      desc:<p>{a.cliente.nome} ha pagato: <Link to="">{a.pietanza.nome}</Link>.</p>
+      desc:<p>{a.d.c_nome} ha pagato: <Link to={`/divisionecontopagamento${role}/${a.d.p_id}`}>{a.d.pz_nome}</Link>.</p>
     }),
   })[a.significato](a);
   const c = ({time: a.created_at, desc: b.desc});
