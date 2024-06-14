e22af74787e28f5aac476a70f5dcd0663ed9ab54
bzn0u95quudr7jutfjap <114179940+bzn0u95quudr7jutfjap@users.noreply.github.com>
2024-06-14 15:45:38 +0200

    query bellissime a prova di tutto

--- app-backend/app/Http/Controllers/Api/NotificheController.php
+++ app-backend/app/Http/Controllers/Api/NotificheController.php
@@ -11,7 +11,90 @@ use DB;
 class NotificheController extends Controller
 {
 
+    private $query_norifiche_ristoratore = <<<'EOF'
+    select * from
+    (
+      (
+          select n.id, n.lettura, n.significato, n.created_at, r.id as r_id, p.id as p_id, c.nome as c_nome, null as pz_nome
+          from notifiche as n
+          inner join prenotazioni as p on n.prenotazione = p.id
+          inner join ristoratori as r on r.id = p.ristoratore
+          inner join inviti as i on i.prenotazione = p.id
+          inner join clients as c on c.id = i.cliente
+          inner join (
+            select p.id as pid, min(i.created_at) as cmin
+            from inviti as i
+            inner join prenotazioni as p on p.id = i.prenotazione
+            group by p.id
+          ) as uniq on i.created_at = uniq.cmin and p.id = uniq.pid
+          where significato = 'PRENOTAZIONE CREATA'
+      )
+      union all
+      (
+          select n.id, n.lettura, n.significato, n.created_at, r.id as r_id, p.id as p_id, null as c_nome, null as pz_nome
+          from notifiche as n
+          inner join prenotazioni as p on n.prenotazione = p.id
+          inner join ristoratori as r on r.id = p.ristoratore
+          where significato = 'PRENOTAZIONE CONTO'
+      )
+      union all
+      (
+          select n.id, n.lettura, n.significato, n.created_at, r.id as r_id, p.id as p_id, c.nome as c_nome, pz.nome as pz_nome
+          from notifiche as n
+          inner join ordinazioni as o on n.ordinazione = o.id
+          inner join inviti as i on o.invito = i.id
+          inner join prenotazioni as p on i.prenotazione = p.id
+          inner join ristoratori as r on r.id = p.ristoratore
+          inner join clients as c on c.id = i.cliente
+          inner join pietanze as pz on pz.id = o.pietanza
+          where significato = 'ORDINAZIONE PAGATA'
+          or significato = 'ORDINAZIONE CREATA'
+      )
+      union all
+      (
+          select n.id, n.lettura, n.significato, n.created_at, r.id as r_id, p.id as p_id, c.nome as c_nome, null as pz_nome
+          from notifiche as n
+          inner join inviti as i on n.invito = i.id
+          inner join prenotazioni as p on i.prenotazione = p.id
+          inner join ristoratori as r on r.id = p.ristoratore
+          inner join clients as c on c.id = i.cliente
+          where significato = 'INVITO PAGATO'
+      )
+    ) as notifiche_ristoratori
+    where r_id = ?
+    order by created_at desc
+    EOF;
+
+    private $query_norifiche_cliente = <<<'EOF'
+    select * from (
+      (
+        select n.id, n.lettura, n.significato, n.created_at, c.id as c_id, p.id as p_id, null as i_nome, r.nome as r_nome
+        from notifiche as n
+        inner join prenotazioni as p on p.id = n.prenotazione
+        inner join inviti as i on p.id = i.prenotazione
+        inner join clients as c on c.id = i.cliente
+        inner join ristoratori as r on r.id = p.ristoratore
+        where n.significato = 'PRENOTAZIONE STATO'
+      )
+      union all
+      (
+        select n.id, n.lettura, n.significato, n.created_at, c.id as c_id, p.id as p_id, invitato.nome as i_nome, null as r_nome
+        from notifiche as n
+        inner join inviti as i on i.id = n.invito
+        inner join clients as invitato on invitato.id = i.cliente
+        inner join prenotazioni as p on i.prenotazione = p.id
+        inner join inviti as ii on p.id = ii.prenotazione
+        inner join clients as c on c.id = ii.cliente
+        where n.significato = 'INVITO ACCETTATO'
+        and c.id != invitato.id
+      )
+    ) as notifiche_clienti
+    where c_id = ?
+    order by created_at desc
+    EOF;
+
     private function _notifiche($id,$tipo){
+        $id_e = $id;
         $return = DB::select('select * from notifiche');
         $getbyprenotazione = fn ($id) => DB::select(<<<'EOF'
             select
@@ -32,6 +115,19 @@ class NotificheController extends Controller
             where p.id = ?
             limit 1
         EOF,[$id]);
+        $getbyprenotazionestato = fn ($id) => DB::select(<<<'EOF'
+            select
+                r.id as r_id,
+                c.id as c_id,
+                r.nome as r_nome,
+                c.nome as c_nome
+            from prenotazioni as p
+            inner join inviti as i on p.id = i.prenotazione
+            inner join clients as c on c.id = i.cliente
+            inner join ristoratori as r on r.id = p.ristoratore
+            where p.id = ? and c.id = ?
+            limit 1
+        EOF,[$id, $id_e]);
         $getbyinvitoaccettato = fn ($id, $ic) => DB::select(<<<'EOF'
             select
                 -1 as r_id,
@@ -42,7 +138,7 @@ class NotificheController extends Controller
             inner join clients as iv on iv.id = i.cliente
             inner join prenotazioni as p on p.id = i.prenotazione
             inner join inviti as iiv on iiv.prenotazione = p.id
-            where i.id = ? and iiv.id = ?
+            where i.id = ? and iiv.id = ? and iv.id != iiv.id
             limit 1
         EOF,[$id, $ic]);
         $getbyinvito = fn ($id) => DB::select(<<<'EOF'
@@ -77,15 +173,16 @@ class NotificheController extends Controller
         $appendreturn = function ($a,$v) { $a->d = $v; return $a;};
         $return = array_map(fn ($a) => match($a->significato) {
             'PRENOTAZIONE CREATA'    => $appendreturn($a,$getbyprenotazione($a->prenotazione)),
-            'PRENOTAZIONE STATO'     => $appendreturn($a,$getbyprenotazione($a->prenotazione)),
-            'PRENOTAZIONE CONTO'     => $appendreturn($a,$getbyprenotazione($a->prenotazione)),
-            'PRENOTAZIONE CANCELLATA'=> $appendreturn($a,$getbyprenotazione($a->prenotazione)),
+            'PRENOTAZIONE STATO'     => $appendreturn($a,$getbyprenotazionestato($a->prenotazione)),
+            'PRENOTAZIONE CONTO'     => $appendreturn($a,$getbyprenotazionestato($a->prenotazione)),
+            'PRENOTAZIONE CANCELLATA'=> $appendreturn($a,$getbyprenotazionestato($a->prenotazione)),
             'INVITO ACCETTATO'       => $appendreturn($a,$getbyinvitoaccettato($a->invito, $id)),
             'INVITO PAGATO'          => $appendreturn($a,$getbyinvito($a->invito)),
             'ORDINAZIONE CREATA'     => $appendreturn($a,$getbyordinazione($a->ordinazione)),
             'ORDINAZIONE CANCELLATA' => $appendreturn($a,$getbyordinazione($a->ordinazione)),
             'ORDINAZIONE PAGATA'     => $appendreturn($a,$getbyordinazione($a->ordinazione)),
         },$return);
+        print_r(['tipo' => $tipo, 'data' => $return]);
         $return = array_filter($return, fn ($a) => $a->d);
         foreach($return as &$a){
             $a->d = $a->d[0];
@@ -106,7 +203,9 @@ class NotificheController extends Controller
     }
 
     public function count($tipo,$id){
+        ob_start();
         $return = count($this->_notifiche($id,$tipo));
+        ob_end_clean();
         return response()->json(['count' => $return], 200);
     }
 }
