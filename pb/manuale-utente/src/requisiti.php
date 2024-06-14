0cfdba15a631dd6e4273fa2ebfe88bd1cba2c0ec
albyx97 <148105963+albyx97@users.noreply.github.com>
2024-06-14 11:33:27 +0200

    Rimosso supporto tecnico da requisiti.php

--- pb/manuale-utente/src/requisiti.php
+++ pb/manuale-utente/src/requisiti.php
@@ -60,25 +60,3 @@ Mozilla Firefox & 115.0 \\
 Safari & 17.0 \\ 
 \end{longtblr}
 \end{center}
-
-\section{Supporto tecnico}
-
-Per eventuali malfunzionamenti riscontrati durante l’utilizzo dell’applicazione, si prega di contattare il supporto tecnico inviando una mail all’indirizzo:
-
-\begin{center}
-    \textbf{sweet16.unipd@gmail.com}
-\end{center}
-
-È consigliato il seguente formato per la mail:
-\begin{itemize}
-  \item Oggetto: ``nome dell’evento da segnalare'';
-  \item Corpo:
-  \begin{itemize}
-    \item Data in cui si è riscontrato il malfunzionamento;
-    \item Descrizione del malfunzionamento in esame;
-    \item Sistema operativo e browser in cui si è verificato il problema.
-  \end{itemize}
-  \item Allegato (facoltativo): immagini utili per la descrizione del malfunzionamento.
-\end{itemize}
-
-
