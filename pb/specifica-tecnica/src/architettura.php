e1b3df0dead4154ad804b1fe37358f823a98c069
bzn0u95quudr7jutfjap <114179940+bzn0u95quudr7jutfjap@users.noreply.github.com>
2024-06-16 16:32:49 +0200

    aggiunti esempi spiegazioni conditional renderin e compound components

--- pb/specifica-tecnica/src/architettura.php
+++ pb/specifica-tecnica/src/architettura.php
@@ -1,14 +1,14 @@
 \pagebreak
 \section{Architettura}
 
-\subsection{Architettura Front-end}
-
-\subsubsection{Introduzione}
-
 L’architettura del prodotto \textit{Easy Meal} non segue un pattern architetturale specifico, poiché nessuno di essi soddisfa appieno le esigenze di modularità e scalabilità dell’applicazione. 
 Invece, si è scelto di utilizzare una combinazione di design pattern tipici della libreria ReactJS, selezionati e adattati secondo le specifiche necessità del progetto. 
 Questo approccio permette di garantire la separazione della logica di business tra le varie componenti, semplificando la gestione degli stati dell’applicazione. \\
 Ogni design pattern adottato è descritto dettagliatamente nelle sezioni successive. \\
+
+\subsection{Architettura Front-end}
+
+\subsubsection{Introduzione}
 
 Ogni pagina dell'applicazione utilizza chiamate alle API per recuperare i dati necessari alla sua visualizzazione. 
 Inoltre, sfrutta gli hooks forniti da React, come useState e useEffect, per gestire lo stato dell'applicazione e aggiornare dinamicamente la UI. 
@@ -16,21 +16,34 @@ Grazie a questo approccio, l'applicazione è in grado di fornire un'esperienza u
 Le singole chiamate alle API permettono di recuperare i dati in modo efficiente, sicuro e scalabile, separando la logica di business dall’interfaccia utente. 
 L'uso degli hook consente di gestire lo stato dell’applicazione in modo dichiarativo e modulare, semplificando la leggibilità del codice e la gestione dei dati.
 
-\subsubsection{Diagrammi delle classi}
-
-DA FARE UNA VOLTA TERMINATA LA CODIFICA
-
 \subsubsection{Design Pattern utilizzati}
 
-In questa sezione, vengono descritti i design pattern utilizzati nell’applicazione basata su React. Sono state adottate diverse soluzioni per modularizzare le singole componenti e adattarle alle specifiche esigenze di realizzazione ed integrazione previste. \\
+In questa sezione, vengono descritti i design pattern utilizzati nell’applicazione basata su React. Sono state adottate diverse soluzioni per modulare le singole componenti e adattarle alle specifiche esigenze di realizzazione ed integrazione previste. \\
 In particolare, possiamo dettagliare l’utilizzo di:
-\begin{itemize}
-\item \textbf{React Hook}: utilizzati per gestire lo stato dell’applicazione in modo efficiente e visualizzare dinamicamente le informazioni. 
-Vengono impiegati sia gli hook nativi di React (come useState, useEffect e useContext), sia hook personalizzati creati in base alle esigenze delle singole viste e componenti, come precedentemente descritto.
-\item \textbf{Conditional Rendering}: permette di mostrare contenuti diversi in base a determinate condizioni. Vengono sviluppati componenti in grado di verificare suddette condizioni e rendere visibili i dati pertinenti in base a queste.
-\item \textbf{Compound Components}: consentono di modularizzare le singole componenti attraverso una gerarchia padre-figlio, dove un componente padre contiene uno o più componenti figlio. 
-Questo approccio permette di specializzare la gestione dei dati e personalizzare l’interfaccia utente in modo centralizzato, seguendo una lista di opzioni unica.
-\end{itemize}
+
+<?php
+
+$a = [
+  'React Hook' => <<<EOF
+  Utilizzati per gestire lo stato dell’applicazione in modo efficiente e visualizzare dinamicamente le informazioni. Vengono impiegati sia gli hook nativi di React (come useState, useEffect e useContext), sia hook personalizzati creati in base alle esigenze delle singole viste e componenti, come precedentemente descritto.
+  EOF,
+
+  'Conditional Rendering' => <<<EOF
+  Permette di mostrare contenuti diversi in base a determinate condizioni. Vengono sviluppati componenti in grado di verificare suddette condizioni e rendere visibili i dati pertinenti in base a queste.
+
+  L'esempio migliore di questo pattern si trova nel Layout, dove in base al ruolo dell'attore utilizzante il prodotto, l'header cambia al fine di mostrare le funzionalità disponibili quell'attore.
+  EOF,
+
+  'Compound Components' => <<<EOF
+  Consentono di modulare le singole componenti attraverso una gerarchia padre-figlio, dove un componente padre contiene uno o più componenti figlio. Questo approccio permette di specializzare la gestione dei dati e personalizzare l’interfaccia utente in modo centralizzato, seguendo una lista di opzioni unica.
+
+  L'esempio migliore di questo pattern si trova di nuovo nel Layout in sinergia con il router, dato un url il router compone la pagina aggregando assieme diversi componenti. Ad esempio la lista di ristoranti viene visualizzata sia da utenti anonimi sia da clienti che stanno effettuando una prenotazione, mentre non può essere visualizzata dal ristoratore.
+  EOF,
+];
+
+echo implode("\n\n\n", array_map(fn ($k,$a) => "\\subsubsubsection{$k} $a",array_keys($a),$a));
+
+?>
 
 \subsection{Architettura Back-end}
 
@@ -45,7 +58,6 @@ MySQL si integra perfettamente con Laravel, permettendo di sfruttare al meglio l
 Le singole componenti del sistema sono strutturate come segue:
 \begin{itemize}
 \item \textbf{Laravel}: Utilizzato per gestire la logica di business, l'autenticazione, la gestione delle rotte e la comunicazione con il database.
-Laravel offre una struttura MVC (Model-View-Controller) che separa chiaramente la logica di business dalla presentazione e dalla gestione dei dati.
 \item \textbf{MySQL}: Utilizzato per la persistenza dei dati, MySQL offre una soluzione robusta e scalabile per gestire le informazioni necessarie all'applicazione. Grazie all'ORM Eloquent di Laravel, è possibile interagire con il database in modo intuitivo e efficiente.
 \item \textbf{API RESTful}: Le API sono implementate seguendo il pattern RESTful, che consente una chiara separazione dei dati tra client e server. 
 Le API RESTful permettono di esporre le risorse dell'applicazione tramite HTTP, rendendo possibile la comunicazione sincrona tra il client e il backend.
