bc19d83a1919a35f691b49eb5eb51b7e0000e9db
albyx97 <148105963+albyx97@users.noreply.github.com>
2024-06-20 11:14:14 +0200

    Inserite le macrosezioni di istruzioni all'uso

--- pb/manuale-utente/src/3-istruzioniuso.php
+++ pb/manuale-utente/src/3-istruzioniuso.php
@@ -1,4 +1,14 @@
 \pagebreak
 \section{Istruzioni all'uso}
 
+\subsection{Accesso}
+
+// DA FARE TERMINATA LA CODIFICA
+
+\subsection{Cliente}
+
+// DA FARE TERMINATA LA CODIFICA
+
+\subsection{Ristoratore}
+
 // DA FARE TERMINATA LA CODIFICA
