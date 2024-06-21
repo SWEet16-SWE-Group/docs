266b813ad6b1ef911ebabe00f42c25e965e2a36c
bzn0u95quudr7jutfjap <114179940+bzn0u95quudr7jutfjap@users.noreply.github.com>
2024-06-21 13:46:53 +0200

    finito le istruzioni più inutili e barebone che si potessero fare

--- pb/manuale-utente/src/3-istruzioniuso.php
+++ pb/manuale-utente/src/3-istruzioniuso.php
@@ -59,7 +59,7 @@ $contenuti = [
     Selezionato un profilo cliente da utilizzare, ci troviamo nella Dashboard Cliente.
     Qui sono stampate le prenotazioni in ordine di data con delle informazioni base.
 
-    IMG-DASHBOARD-CLIENTE
+    IMG-DASHBOARD-C
 
     EOF,
     'Prenotazioni' => <<<'EOF'
@@ -91,12 +91,13 @@ $contenuti = [
 
     Poi seguono i link per la gestione dei pagamenti e la navigazione del menu per effettuare ordinazioni.
 
-    IMG-DETTAGLI-CLIENTE
+    IMG-DETTAGLI-C
 
     EOF,
     'Ordinazioni' => <<<'EOF'
 
     Selezionato l'opzione 'ordina' sotto ai dettagli della prenotazione si viene portati al menù del ristorante.
+    N.B.: l'opzione 'ordina' è disponibile solo dopo che il ristoratore ha accettato la prenotazione.
     Ogni pietanza sarà accompagnata dai propri ingredienti e un pulsante di ordinazione.
     Facendo click su di esso si viene portati al form dove poter scegliere gli ingredienti da togliere o aggiungere,
     e il numero di ordinazioni da effettuare con quelle personalizzazioni.
@@ -120,8 +121,28 @@ $contenuti = [
   'Ristoratore' =>
   [
     'Dashboard' => <<<'EOF'
+
+    Tornando alla selezione dei profili e selezionato un profilo ristoratore da utilizzare, ci troviamo nella Dashboard Ristoratore.
+    Qui sono stampate:
+    \begin{itemize}
+    \item Le informazioni del ristorante
+    \item Le prenotazioni in ordine di data con delle informazioni base
+    \item I link alle pagine di gestione del menù
+    \end{itemize}
+
+    Le prenotazioni sono accompagnate anche dai tasti di accettazione, rifiuto, esaminazione dei pagamenti ed esaminazione dettagli aggiuntivi.
+
+    IMG-DASHBOARD-R
+
     EOF,
     'Dettagli' => <<<'EOF'
+
+    Nei dettagli si mostrano prima i dettagli base della prenotazione.
+    Di seguito ci sono le quantità di ingredienti grezzi necessari a soddisfare la prenotazione con le ordinazioni di ogni cliente.
+    Le ordinazioni possono essere cancellate in via eccezionale.
+
+    IMG-DETTAGLI-R
+
     EOF,
     'Gestione del menù' => <<<'EOF'
 
