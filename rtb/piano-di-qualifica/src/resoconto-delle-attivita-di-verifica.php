8528766de945561c88ed83723a3cf259d3feb0d6
bzn0u95quudr7jutfjap <114179940+bzn0u95quudr7jutfjap@users.noreply.github.com>
2024-06-22 00:01:10 +0200

    .6

--- rtb/piano-di-qualifica/src/resoconto-delle-attivita-di-verifica.php
+++ rtb/piano-di-qualifica/src/resoconto-delle-attivita-di-verifica.php
@@ -4,25 +4,25 @@
 \subsection{Fornitura}
 
 \subsubsection{MPC-AC e MPC-ETC: Actual Cost e Estimated to Completion}
-\begin{figure}[H] \includegraphics[scale=1]{i0.png} \end{figure}
+\begin{figure}[H] \includegraphics[scale=.6]{i0.png} \end{figure}
 
 \subsubsection{MPC-EV e MPC-PV: Earned Value e Planned Value}
-\begin{figure}[H] \includegraphics[scale=1]{i1.png} \end{figure}
+\begin{figure}[H] \includegraphics[scale=.6]{i1.png} \end{figure}
 
 \subsubsection{MPC-SV: Schedule Variance}
-\begin{figure}[H] \includegraphics[scale=1]{i2.png} \end{figure}
+\begin{figure}[H] \includegraphics[scale=.6]{i2.png} \end{figure}
 
 \subsubsection{MPC-CV: Cost Variance}
-\begin{figure}[H] \includegraphics[scale=1]{i3.png} \end{figure}
+\begin{figure}[H] \includegraphics[scale=.6]{i3.png} \end{figure}
 
 \subsubsection{MPC-EAC: Estimated at Completion}
-\begin{figure}[H] \includegraphics[scale=1]{i4.png} \end{figure}
+\begin{figure}[H] \includegraphics[scale=.6]{i4.png} \end{figure}
 
 \subsection{Documentazione}
 
 \subsubsection{MPC-IG: Indice Gulpease}
-\begin{figure}[H] \includegraphics[scale=1]{d0.png} \end{figure}
+\begin{figure}[H] \includegraphics[scale=.6]{d0.png} \end{figure}
 
 \subsubsection{MPC-CO: Correttezza Ortografica}
-\begin{figure}[H] \includegraphics[scale=1]{d1.png} \end{figure}
+\begin{figure}[H] \includegraphics[scale=.6]{d1.png} \end{figure}
 
