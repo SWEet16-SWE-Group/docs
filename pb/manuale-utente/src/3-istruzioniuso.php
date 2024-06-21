c7d4837d03f86a0c8249ff4ff747b0dd87176ec9
bzn0u95quudr7jutfjap <114179940+bzn0u95quudr7jutfjap@users.noreply.github.com>
2024-06-21 11:46:04 +0200

    struttura di anonimo e autenticato

--- pb/manuale-utente/src/3-istruzioniuso.php
+++ pb/manuale-utente/src/3-istruzioniuso.php
@@ -3,23 +3,54 @@
 $contenuti = [
   'Utente anonimo' =>
   [
-    'Ricerca dei ristoranti' =>
-    [
-      'Elenco dei ristoranti' => null,
-      'Homepage del ristorante' => null,
-      'Menù del ristorante' => null,
-      'Form di prenotazione' => null,
-    ],
+    'Esplorazione del sito' => <<<'EOF'
+
+    Come utente anonimo, vale a dire, senza aver eseguito l'accesso, è possibile esplorare il sito.
+    L'esplorazione comprende la ricerca e dei ristoranti con annessi i rispettivi menù.
+
+    IMG-RISTORANTI
+    IMG-RISTORANTE
+    IMG-MENU
+    
+    EOF,
+    'Registrazione e accesso' => <<<'EOF'
+
+    Conclusa l'esplorazione del sito, l'utente può registrarsi tramite l'uso di email e password.
+    Oppure può accedere direttamente se già in possesso di un account.
+
+    IMG-REGISTRAZIONE
+    IMG-ACCESSO
+
+    EOF,
+
   ],
   'Utente autenticato' =>
   [
-    'Navbar' =>
-    [
-      'Impostazioni account' => null,
-      'Nuovo profilo cliente' => null,
-      'Nuovo profilo ristoratore' => null,
-    ],
-    'Selezione profili' => null,
+    'Selezione profili' => <<<'EOF'
+
+    Una volta eseguito l'accesso ci troveremo nella prima dashboard. Questa è la Dashboard Account.
+    Da qui è possibile creare nuovi profili Cliente o Ristorare, accedere o modificare quelli già esistenti.
+
+    IMG-SELEZIONE
+
+    Creazione/modifica di un cliente.
+
+    IMG-CREAZIONE-CLIENTE
+    IMG-MODIFICA-CLIENTE
+
+    Creazione/modifica di un ristoratore.
+
+    IMG-CREAZIONE-RISTORATORE
+    IMG-MODIFICA-RISTORATORE
+
+    EOF,
+    'Modifica dell\'account' => <<<'EOF'
+
+    Per la protezione degli account è possibile cambiare la propria mail o password in qualsiasi momento.
+
+    IMG-MODIFICA-ACCOUNT
+
+    EOF,
   ],
   'Cliente' =>
   [
