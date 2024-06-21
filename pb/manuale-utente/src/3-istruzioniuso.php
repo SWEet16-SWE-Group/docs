1b8ad698553239a99e1c9bf1488a83ba8f34be09
bzn0u95quudr7jutfjap <114179940+bzn0u95quudr7jutfjap@users.noreply.github.com>
2024-06-21 09:44:11 +0200

    spostato lo scheletro delle istruzioni d'uso nel secondo file

--- pb/manuale-utente/src/3-istruzioniuso.php
+++ pb/manuale-utente/src/3-istruzioniuso.php
@@ -1,14 +1,84 @@
-\pagebreak
-\section{Istruzioni all'uso}
+<?php
 
-\subsection{Accesso}
+$contenuti = [
+  'Utente anonimo' =>
+  [
+    'Ricerca dei ristoranti' =>
+    [
+      'Elenco dei ristoranti' => null,
+      'Homepage del ristorante' => null,
+      'Menù del ristorante' => null,
+      'Form di prenotazione' => null,
+    ],
+  ],
+  'Utente autenticato' =>
+  [
+    'Navbar' =>
+    [
+      'Impostazioni account' => null,
+      'Nuovo profilo cliente' => null,
+      'Nuovo profilo ristoratore' => null,
+    ],
+    'Selezione profili' => null,
+  ],
+  'Cliente' =>
+  [
+    'Navbar' =>
+    [
+      'Selezione profili' => null,
+      'Impostazioni profilo' => null,
+      'Dashboard' => null,
+      'Ricerca dei ristoranti' =>
+      [
+        'Elenco dei ristoranti' => null,
+        'Homepage del ristorante' => null,
+        'Menù del ristorante' => null,
+        'Form di prenotazione' => null,
+      ],
+      'Notifiche' => null,
+    ],
+    'Dashboard' =>
+    [
+      'Lista di prenotazioni' =>
+      [
+        'Attive' => null,
+        'Scadute' => null,
+      ],
+      'Prenotazione' =>
+      [
+        'Ordinazioni collaborative' => null,
+        'Manipolare un\'ordinazione' => null,
+        'Effettuare pagamenti' => null,
+        // 'Lasciare una recensione' => null,
+      ],
+    ],
+  ],
+  'Ristoratore' =>
+  [
+    'Navbar' =>
+    [
+      'Selezione profili' => null,
+      'Impostazioni profilo' => null,
+      'Dashboard' => null,
+      'Impostazioni menù' =>
+      [
+        'Manipolazione pietanze' => null,
+      ],
+      'Notifiche' => null,
+    ],
+    'Dashboard' =>
+    [
+      'Lista di prenotazioni' => null,
+      'Prenotazione' =>
+      [
+        'Ordinazioni collaborative' => null,
+        'Dettagli ordinazioni' => null,
+        'Segna il pagamento di pietanze/clienti come effettuati' => null,
+      ],
+    ],
+  ],
+];
 
-// DA FARE TERMINATA LA CODIFICA
+echo _stampatex($contenuti, 'Manuale utente', 'section');
 
-\subsection{Cliente}
-
-// DA FARE TERMINATA LA CODIFICA
-
-\subsection{Ristoratore}
-
-// DA FARE TERMINATA LA CODIFICA
+?>
